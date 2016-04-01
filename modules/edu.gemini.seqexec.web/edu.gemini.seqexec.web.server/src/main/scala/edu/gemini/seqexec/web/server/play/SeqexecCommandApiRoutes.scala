1c1cbb27aa5e569c05706daada3cd9bbec97d366
Carlos Quiroz <cquiroz@gemini.edu>
2016-04-01 14:11:31 -0300

    Return command responses as JSON

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/play/SeqexecCommandApiRoutes.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/play/SeqexecCommandApiRoutes.scala
@@ -5,10 +5,17 @@ import play.api.mvc.{Action, Results}
 import play.api.routing.Router.Routes
 import play.api.routing.sird._
 
+import upickle.default._
+
+case class CommandResponse(command: String, response: String)
+
+/**
+  * Define routes for command-line utilities that want to talk to the seqexec server
+  */
 object SeqexecCommandApiRoutes {
   val routes: Routes = {
     case GET(p"/api/seqexec/commands/host") => Action {
-      Results.Ok(Commands().seq("host", Array.empty[String]))
+      Results.Ok(write(CommandResponse("host", Commands().seq("host", Array.empty[String]))))
     }
   }
 
