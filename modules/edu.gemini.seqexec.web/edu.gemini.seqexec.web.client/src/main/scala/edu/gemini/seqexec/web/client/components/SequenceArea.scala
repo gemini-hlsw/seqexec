35ed14a764293b741801e194321667cd5b2abc27
Carlos Quiroz <cquiroz@gemini.edu>
2016-04-12 16:20:52 -0300

    Added component to hold the tabs and initialize tab mechanism

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/SequenceArea.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/SequenceArea.scala
@@ -1,5 +1,7 @@
 package edu.gemini.seqexec.web.client.components
 
+import edu.gemini.seqexec.web.client.components.TabularMenu.TabItem
+import edu.gemini.seqexec.web.client.semanticui._
 import edu.gemini.seqexec.web.client.semanticui.elements.button.Button
 import japgolly.scalajs.react.ReactComponentB
 import japgolly.scalajs.react.vdom.prefix_<^._
@@ -14,24 +16,10 @@ object SequenceArea {
         TextMenuSegment("Running Sequences"),
         <.div(
           ^.cls := "ui bottom attached segment",
-          <.div(
-            ^.cls := "ui attached tabular menu",
-            <.a(
-              ^.cls := "active item",
-              "GPI (GS-2016A-Q-0-1)"
-            ),
-            <.a(
-              ^.cls := "item",
-              "GMOS-S (GS-2016A-Q-5-3)"
-            ),
-            <.a(
-              ^.cls := "item negative",
-              IconAttention,
-              "F2 (GS-2016A-Q-4-1)"
-            )
-          ),
+          TabularMenu(List(TabItem("GPI (GS-2016A-Q-0-1)", isActive = true, "GPI"), TabItem("GMOS-S (GS-2016A-Q-5-3)", isActive = false, "GMOS_S"), TabItem("F2 (GS-2016A-Q-4-1)", isActive = false, "F2"))),
           <.div(
             ^.cls := "ui bottom attached active tab segment",
+            dataTab := "GPI",
             <.div(
               ^.cls := "ui grid",
               <.div(
@@ -114,6 +102,100 @@ object SequenceArea {
                               ^.cls := "collapsing right aligned",
                               IconPause,
                               " ",
+                              IconCaretRight
+                            )
+                          )
+                        )
+                      )
+                    )
+                  )
+                )
+              ),
+              <.div(
+                ^.cls := "row computer only",
+                <.div(
+                  ^.cls := "sixteen wide column",
+                  <.div(
+                    ^.cls := "ui raised secondary segment",
+                    <.h4("Log"),
+                    <.div(
+                      ^.cls := "ui form",
+                      <.div(
+                        ^.cls := "field",
+                        <.textarea()
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          ),
+          <.div(
+            ^.cls := "ui bottom attached tab segment",
+            dataTab := "GMOS_S",
+            <.div(
+              ^.cls := "ui grid",
+              <.div(
+                ^.cls := "row",
+                <.div(
+                  ^.cls := "four wide column tablet computer only",
+                  <.div(
+                    ^.cls := "ui raised secondary segment",
+                    <.h4("Headers"),
+                    <.div(
+                      ^.cls := "ui form",
+                      <.div(
+                        ^.cls := "required field",
+                        <.label("Observer"),
+                        <.input(
+                          ^.`type` :="text",
+                          ^.autoComplete :="off"
+                        )
+                      ),
+                      <.div(
+                        ^.cls := "required field",
+                        <.label("SOS"),
+                        <.input(
+                          ^.`type` :="text",
+                          ^.autoComplete :="off"
+                        )
+                      ),
+                      DropdownMenu(DropdownMenu.Props("Image Quality", "Select", List("IQ20", "IQ70", "IQ85", "Any"))),
+                      DropdownMenu(DropdownMenu.Props("Cloud Cover", "Select", List("CC20", "CC50", "CC70", "CC80", "CC90", "Any"))),
+                      DropdownMenu(DropdownMenu.Props("Water Vapor", "Select", List("WV20", "WV50", "WV80", "Any"))),
+                      DropdownMenu(DropdownMenu.Props("Sky Background", "Select", List("SB20", "SB50", "SB80", "Any")))
+                    )
+                  )
+                ),
+                <.div(
+                  ^.cls := "twelve wide computer twelve wide tablet sixteen column",
+                  <.div(
+                    ^.cls := "ui raised secondary segment",
+                    <.div(
+                      ^.cls := "row",
+                      Button("Run"),
+                      Button("Pause")
+                    ),
+                    <.div(
+                      ^.cls := "ui divider"
+                    ),
+                    <.div(
+                      ^.cls := "row",
+                      <.table(
+                        ^.cls := "ui selectable compact celled table",
+                        <.thead(
+                          <.tr(
+                            <.th("Step"),
+                            <.th("State"),
+                            <.th("Config")
+                          )
+                        ),
+                        <.tbody(
+                          <.tr(
+                            <.td("1"),
+                            <.td("Done"),
+                            <.td(
+                              ^.cls := "collapsing right aligned",
                               IconCaretRight
                             )
                           )
---
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/semanticui/package.scala
@@ -0,0 +1,7 @@
+package edu.gemini.seqexec.web.client
+
+import japgolly.scalajs.react.vdom.prefix_<^._
+
+package object semanticui {
+  val dataTab   = "data-tab"  .reactAttr
+}
