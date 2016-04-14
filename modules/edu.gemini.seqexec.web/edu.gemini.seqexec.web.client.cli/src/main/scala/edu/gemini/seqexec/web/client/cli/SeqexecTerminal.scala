cd85f2bbbd079532ea04cf3eb9d7a18a54415287
Carlos Quiroz <cquiroz@gemini.edu>
2016-04-14 15:21:56 -0300

    Support the run command on the terminal

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client.cli/src/main/scala/edu/gemini/seqexec/web/client/cli/SeqexecTerminal.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client.cli/src/main/scala/edu/gemini/seqexec/web/client/cli/SeqexecTerminal.scala
@@ -26,30 +26,32 @@ object SeqexecTerminal extends js.JSApp {
     val baseUrl = "/api/seqexec/commands"
     def handle(args: List[String], terminal: Terminal):Unit
 
-    def complete[A <: dom.XMLHttpRequest](terminal: Terminal): PartialFunction[Try[A], js.Any] = {
+    def complete[A <: dom.XMLHttpRequest, B](terminal: Terminal, f: CliCommand => B): PartialFunction[Try[A], js.Any] = {
       case Success(s) =>
         val r = read[CliCommand](s.responseText)
-        if (r.error) terminal.error(r.response) else terminal.echo(r.response)
+        if (r.error) terminal.error(r.response) else terminal.echo(f(r).toString)
       case Failure(s) => terminal.error(s.toString)
     }
 
     def pause(t: Terminal) = Future.apply(t.pause())
     def resume(t: Terminal) = Future.apply(t.resume())
 
-    def runInBackground[A <: dom.XMLHttpRequest](a: => Future[A], t: Terminal): Unit =
+    def runInBackground[A <: dom.XMLHttpRequest, B](a: => Future[A], f: CliCommand => B, t: Terminal): Unit =
       (for {
         _ <- pause(t)
-        f <- a.andThen(complete(t))
+        f <- a.andThen(complete(t, f))
       } yield f).andThen {
         case u => resume(t) // resume regardless of whether there is an error
       }
   }
 
+  def defaultResponse(c: CliCommand) = c.response
+
   case class Command(cmd: String, args: Int, handler: CommandHandler, description: String)
 
   object HostHandler extends CommandHandler {
     override def handle(args: List[String], terminal: Terminal): Unit = {
-      runInBackground(Ajax.get(s"$baseUrl/host"), terminal)
+      runInBackground(Ajax.get(s"$baseUrl/host"), defaultResponse, terminal)
     }
   }
 
@@ -57,14 +59,21 @@ object SeqexecTerminal extends js.JSApp {
     override def handle(args: List[String], terminal: Terminal): Unit = {
       runInBackground(Ajax.post(s"$baseUrl/host",
         data = s"host=${args.head}",
-        headers = Map("Content-Type" -> "application/x-www-form-urlencoded")), terminal)
+        headers = Map("Content-Type" -> "application/x-www-form-urlencoded")), defaultResponse, terminal)
+    }
+  }
+
+  object RunHandler extends CommandHandler {
+    override def handle(args: List[String], terminal: Terminal): Unit = {
+      runInBackground(Ajax.post(s"$baseUrl/${args.head}/run"), defaultResponse, terminal)
     }
   }
 
   // Maps the command text and amount of args required to command handler
   val commands: List[Command] = List(
     Command("host", 0, HostHandler, "[[b;;]host]: Returns the odb host used by the seqexec"),
-    Command("host", 1, SetHostHandler, "[[b;;]host] [[b;;]host:port]: Sets the odb host:port used by the seqexec")
+    Command("host", 1, SetHostHandler, "[[b;;]host] [[b;;]host:port]: Sets the odb host:port used by the seqexec"),
+    Command("run", 1, RunHandler, "[[b;;]run obsId]: Runs obs id")
   )
 
   // Used for tab completion
@@ -74,11 +83,13 @@ object SeqexecTerminal extends js.JSApp {
     val tokens = command.split(" ").toList
 
     def find(cmd: String, args: List[String]): Option[Command] = commands.find(c => c.cmd === cmd && c.args == args.size)
+    def findSimilar(cmd: String): Option[Command] = commands.find(c => c.cmd === cmd)
 
     tokens match {
       case cmd :: args if find(cmd, args).isDefined              => find(cmd, args).foreach(_.handler.handle(args, terminal))
+      case cmd :: args if findSimilar(cmd).isDefined             => findSimilar(cmd).foreach(c => terminal.echo(s"Incomplete command: Usage ${c.description}"))
       case "help" :: Nil                                         => terminal.echo(s"Commands available: ${commands.map(_.cmd).distinct.mkString(" ")}")
-      case "help" :: cmd :: _  if commands.exists(_.cmd === cmd) => terminal.echo(s"help:\n${commands.filter(_.cmd === cmd).map(_.description).mkString("\n")}")
+      case "help" :: cmd :: _  if commands.exists(_.cmd === cmd) => terminal.echo(s"help:\nexit clear ${commands.filter(_.cmd === cmd).map(_.description).mkString("\n")}")
       case "" :: Nil                                             => // Ignore
       case cmd :: _                                              => terminal.error(s"Command '$command' unknown")
     }
