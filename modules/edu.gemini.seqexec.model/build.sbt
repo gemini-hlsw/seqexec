9f3fe701a474ca306cbaa0664e4b15da0a04e8a4
Carlos Quiroz <cquiroz@gemini.edu>
2016-04-22 09:42:39 -0300

    Setup edu.gemini.seqexec.shared module

---
+++ modules/edu.gemini.seqexec.model/build.sbt
@@ -0,0 +1,5 @@
+import Settings.Libraries._
+
+name := "edu.gemini.seqexec.model"
+
+libraryDependencies ++= TestLibs.value
--- project/OcsBuild.scala
+++ project/OcsBuild.scala
@@ -8,6 +8,10 @@ object OcsBuild extends Build {
   // List all the modules and their inter dependencies
   lazy val edu_gemini_seqexec_server = project
     .in(file("modules/edu.gemini.seqexec.server"))
+    .dependsOn(edu_gemini_seqexec_server_model)
+
+  lazy val edu_gemini_seqexec_server_model = project
+    .in(file("modules/edu.gemini.seqexec.model"))
 
   // Only the root is needed for composite web application modules
   lazy val edu_gemini_seqexec_web = project
