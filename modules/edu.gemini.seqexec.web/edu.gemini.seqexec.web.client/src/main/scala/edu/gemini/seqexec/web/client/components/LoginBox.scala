4a377e80213995a1adb1faa2d87af4c3a446bbab
Carlos Quiroz <cquiroz@gemini.edu>
2016-05-30 12:30:42 -0400

    Add login form to the login modal box

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/LoginBox.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/LoginBox.scala
@@ -4,6 +4,7 @@ import diode.react.ModelProxy
 import japgolly.scalajs.react.{Callback, ReactComponentB, ReactDOM}
 import japgolly.scalajs.react.vdom.prefix_<^._
 import edu.gemini.seqexec.web.client.semanticui.SemanticUI._
+import edu.gemini.seqexec.web.client.semanticui.elements.icon.Icon.{IconUser, IconLock}
 import edu.gemini.seqexec.web.client.model.{SectionOpen, SectionVisibilityState}
 
 /**
@@ -15,14 +16,65 @@ object LoginBox {
 
   val component = ReactComponentB[Props]("Login")
     .stateless
-    .render_P(p => {println("Render" + p.open())
+    .render_P(p =>
       <.div(
         ^.cls := "ui modal",
         <.div(
           ^.cls := "header",
-          "Header"
+          "Login"
+        ),
+        <.div(
+          ^.cls := "ui content",
+          <.form(
+            ^.cls :="ui form",
+            <.div(
+              ^.cls :="field",
+              <.label(
+                ^.htmlFor := "username",
+                "Username: "
+              ),
+              <.div(
+                ^.cls :="ui icon input",
+                <.input(
+                  ^.`type` := "text",
+                  ^.placeholder := "Username",
+                  ^.name := "username",
+                  ^.id := "username"
+                ),
+                IconUser
+              )
+            ),
+            <.div(
+              ^.cls :="field",
+              <.label(
+                ^.htmlFor := "password",
+                "Password: "
+              ),
+              <.div(
+                ^.cls :="ui icon input",
+                <.input(
+                  ^.`type` :="password",
+                  ^.placeholder := "Password",
+                  ^.name := "password",
+                  ^.id := "password"
+                ),
+                IconLock
+              )
+            )
+          )
+        ),
+        <.div(
+          ^.cls := "actions",
+            <.div(
+              ^.cls := "ui button",
+              "Cancel"
+            ),
+            <.div(
+              ^.cls := "ui button",
+              "Login"
+            )
         )
-      )}
+      )
     ).componentDidUpdate(s =>
       Callback {
         import org.querki.jquery.$
