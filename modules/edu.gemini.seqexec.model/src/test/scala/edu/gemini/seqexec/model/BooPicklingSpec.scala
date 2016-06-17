ef89bd4c0a542b2c079322053da9e6e4cd407523
Carlos Quiroz <cquiroz@gemini.edu>
2016-06-17 14:01:32 -0400

    Improve pickle/depickle testing of events

--- modules/edu.gemini.seqexec.model/src/test/scala/edu/gemini/seqexec/model/BooPicklingSpec.scala
+++ modules/edu.gemini.seqexec.model/src/test/scala/edu/gemini/seqexec/model/BooPicklingSpec.scala
@@ -10,7 +10,13 @@ import boopickle.Default._
   */
 class BooPicklingSpec extends FlatSpec with Matchers with PropertyChecks {
 
-  "SeqexecConnectionOpenEvent" should "upickle/depickle" in {
+  "NullEvent" should "pickle/depickle" in {
+    Unpickle[NullEvent.type].fromBytes(Pickle.intoBytes(NullEvent)) shouldEqual NullEvent
+    val event: SeqexecEvent = NullEvent
+    Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
+  }
+
+  "SeqexecConnectionOpenEvent" should "pickle/depickle" in {
     Unpickle[SeqexecConnectionOpenEvent].fromBytes(Pickle.intoBytes(SeqexecConnectionOpenEvent(None))) shouldEqual SeqexecConnectionOpenEvent(None)
     // Mark event as the root class to properly encode it
     val event: SeqexecEvent = SeqexecConnectionOpenEvent(None)
@@ -21,27 +27,43 @@ class BooPicklingSpec extends FlatSpec with Matchers with PropertyChecks {
     Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event2)) shouldEqual event2
   }
 
-  /*"SeqexecConnectionCloseEvent" should "upickle/depickle" in {
-      read[SeqexecConnectionCloseEvent.type](write(SeqexecConnectionCloseEvent)) shouldEqual SeqexecConnectionCloseEvent
+  "SeqexecConnectionCloseEvent" should "pickle/depickle" in {
+    Unpickle[SeqexecConnectionCloseEvent.type].fromBytes(Pickle.intoBytes(SeqexecConnectionCloseEvent)) shouldEqual SeqexecConnectionCloseEvent
+    val event: SeqexecEvent = SeqexecConnectionCloseEvent
+    Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
+  }
+
+  "SeqexecConnectionErrorEvent" should "pickle/depickle" in {
+    forAll { (i: String) =>
+      Unpickle[SeqexecConnectionError].fromBytes(Pickle.intoBytes(SeqexecConnectionError(i))) shouldEqual SeqexecConnectionError(i)
+      val event: SeqexecEvent = SeqexecConnectionError(i)
+      Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
+    }
   }
 
   "SequenceStartEvent" should "upickle/depickle" in {
     forAll { (i: String) =>
-      read[SequenceStartEvent](write(SequenceStartEvent(i))) shouldEqual SequenceStartEvent(i)
+      Unpickle[SequenceStartEvent].fromBytes(Pickle.intoBytes(SequenceStartEvent(i))) shouldEqual SequenceStartEvent(i)
+      val event: SeqexecEvent = SequenceStartEvent(i)
+      Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
+    }
+  }
+
+  "SequenceCompletedEvent" should "upickle/depickle" in {
+    forAll { (i: String) =>
+      Unpickle[SequenceCompletedEvent].fromBytes(Pickle.intoBytes(SequenceCompletedEvent(i))) shouldEqual SequenceCompletedEvent(i)
+      val event: SeqexecEvent = SequenceCompletedEvent(i)
+      Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
     }
   }
 
   "StepExecutedEvent" should "upickle/depickle" in {
     forAll { (i: String, count: Int, file: ObsId) =>
       val event = StepExecutedEvent(i, count, count, file)
-      read[StepExecutedEvent](write(event)) shouldEqual event
+      Unpickle[StepExecutedEvent].fromBytes(Pickle.intoBytes(event)) shouldEqual event
+      val event2: SeqexecEvent = StepExecutedEvent(i, count, count, file)
+      Unpickle[SeqexecEvent].fromBytes(Pickle.intoBytes(event2)) shouldEqual event2
     }
   }
-
-  "SeqexecConnectionError" should "upickle/depickle" in {
-    forAll { (i: String) =>
-      read[SeqexecConnectionError](write(SeqexecConnectionError(i))) shouldEqual SeqexecConnectionError(i)
-    }
-  }*/
 
 }
