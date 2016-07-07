2f8e666725ae72e6e428afc8ca208578e144902f
Danny Navarro <j@dannynavarro.net>
2016-07-07 12:00:08 -0400

    Make TcsConfig, InstConfig and Observation types
    They were just type synonyms before.

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/sequence.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/sequence.scala
@@ -9,50 +9,69 @@ object Engine {
 
   type Sequence = List[Step]
 
+  type Action = IO[Result]
+
   sealed trait Result
   case object Done  extends Result
   case object Error extends Result
 
-  case class Step (t: TcsConfig, i: InstConfig, o: Observation)
+  case class Step(t: TcsConfig, i: InstConfig, o: Observation)
 
-  // case class TcsConfig (r: IO[Result])
-  // Haskell's newtype, how to extract inner IO easily?
-  type TcsConfig  = IO[Result]
-  type InstConfig = IO[Result]
-  type Observation= IO[Result]
+  sealed trait HasAction
+  case class TcsConfig(a:Action) extends HasAction
+  case class InstConfig(a:Action) extends HasAction
+  case class Observation(a:Action) extends HasAction
+
+  def action(ha:HasAction): Action = ha match {
+    case TcsConfig(a:Action) => a
+    case InstConfig(a:Action) => a
+    case Observation(a:Action) => a
+  }
 
   def execute(seq: Sequence): IO[Unit] = {
 
     def step(s:Step): IO[Unit] = s match {
-      case Step(tcs: IO[Result], inst: IO[Result], obsv: IO[Result]) => for {
-        r <- concurrently(tcs, inst)
+      case Step(tcs: TcsConfig, inst: InstConfig, obsv: Observation) => for {
+        r <- concurrently(action(tcs), action(inst))
         (tr, ir) = r
-        obr <- obsv
+        obr <- action(obsv)
         } yield ()
       }
     seq.traverse_(step)
   }
 
-  private def concurrently[A,B](a: IO[A], b: IO[B]): IO[(Result,Result)] = ???
+  private def concurrently[A,B](a: IO[A], b: IO[B]): IO[(A, B)] = ???
 
-  // TODO: Can't this be made less ugly?
-  val sequence1 = List(Step(
-      (for { _ <- putStrLn("Start TCS configuration for Step 1")
-             _ <- IO(Thread.sleep(2000))
-             _ <- putStrLn("Complete TCS configuration for Step 1")
-             } yield Done
-      ),
-      (for { _ <- putStrLn("Start instrument configuration for Step 1")
-             _ <- IO(Thread.sleep(2000))
-             _ <- putStrLn("Complete instrument configuration for Step 1")
-                } yield Done
-      ),
-      (for  { _ <- putStrLn("Start observation for Step 1")
+  val sequence1 = {
+    List(
+      Step(
+        (TcsConfig
+           (for
+            { _ <- putStrLn("Start TCS configuration for Step 1")
+              _ <- IO(Thread.sleep(2000))
+              _ <- putStrLn("Complete TCS configuration for Step 1")
+            } yield Done
+           )
+        ),
+        (InstConfig
+           (for
+            { _ <- putStrLn("Start instrument configuration for Step 1")
+              _ <- IO(Thread.sleep(2000))
+              _ <- putStrLn("Complete instrument configuration for Step 1")
+            } yield Done
+           )
+        ),
+        (Observation
+           (for
+            { _ <- putStrLn("Start observation for Step 1")
               _ <- IO(Thread.sleep(5000))
               _ <- putStrLn("Complete observation for Step 1")
-              } yield Done
-      )))
-
+            } yield Done
+           )
+        )
+      )
+    )
+}
   def main(args: Array[String]): Unit = execute(sequence1).unsafePerformIO()
 
 }
