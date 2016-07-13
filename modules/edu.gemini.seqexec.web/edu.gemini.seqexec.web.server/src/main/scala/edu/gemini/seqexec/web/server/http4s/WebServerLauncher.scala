b52f15723379bf39c57e682727c114f926d0c223
Carlos Quiroz <cquiroz@gemini.edu>
2016-07-13 16:58:28 -0400

    Configure the server with the configuration read from the configuration file

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/WebServerLauncher.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/WebServerLauncher.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.util.logging.Logger
 
 import edu.gemini.seqexec.web.server.common.LogInitialization
-import knobs.{ClassPathResource, Config, FileResource, Required}
+import knobs._
 import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
 
@@ -20,25 +20,34 @@ object WebServerLauncher extends ServerApp with LogInitialization {
   // Initialize logger after the configuration
   val logger = Logger.getLogger(getClass.getName)
 
+  case class ServerConfiguration(host: String, port: Int, devMode: Boolean)
+
   // Attempt to get the file or throw an exception if not possible
-  def configurationFile: File = new File(new File(baseDir, "conf"), "app.cfg")
+  val configurationFile: File = new File(new File(baseDir, "conf"), "app.conf")
 
   // Read the config, first attempt the file or default to the classpath file
   val config: Task[Config] = knobs.loadImmutable(
-    knobs.Optional(FileResource(configurationFile)) :: Required(ClassPathResource("app.cfg")) :: Nil)
+    knobs.Optional(FileResource(configurationFile)) :: Required(ClassPathResource("app.conf")) :: Nil)
 
-  // TODO improve configuration style
-  val devMode = true
+  // configuration specific to the web server
+  val serverConf: Task[ServerConfiguration] =
+    config.map { cfg =>
+      val host = cfg.require[String]("web-server.host")
+      val port = cfg.require[Int]("web-server.port")
+      val devMode = cfg.require[String]("mode")
+      ServerConfiguration(host, port, devMode.equalsIgnoreCase("dev"))
+    }
 
   override def server(args: List[String]): Task[Server] = {
-    val port = 9090
-    logger.info(s"Starting web server on port $port")
-    BlazeBuilder.bindHttp(port, "0.0.0.0")
-      .withWebSockets(true)
-      .mountService(StaticRoutes.service(devMode), "/")
-      .mountService(SeqexecCommandRoutes.service, "/api/seqexec/commands")
-      .mountService(SeqexecUIApiRoutes.service, "/api")
-      .start
+    serverConf >>= { conf =>
+      logger.info(s"Start server on ${conf.devMode ? "dev" | "production"} mode")
+      BlazeBuilder.bindHttp(conf.port, conf.host)
+        .withWebSockets(true)
+        .mountService(StaticRoutes.service(conf.devMode), "/")
+        .mountService(SeqexecCommandRoutes.service, "/api/seqexec/commands")
+        .mountService(SeqexecUIApiRoutes.service, "/api")
+        .start
+    }
   }
 
 }
