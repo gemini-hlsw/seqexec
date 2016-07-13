48e1ccf780ad1f33e857a16fd612ad4d1ce65e6b
Carlos Quiroz <cquiroz@gemini.edu>
2016-07-13 16:18:47 -0400

    Read the configuration file at the startup

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/WebServerLauncher.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/WebServerLauncher.scala
@@ -1,18 +1,31 @@
 package edu.gemini.seqexec.web.server.http4s
 
+import java.io.File
 import java.util.logging.Logger
 
 import edu.gemini.seqexec.web.server.common.LogInitialization
+import knobs.{ClassPathResource, Config, FileResource, Required}
 import org.http4s.server.Server
 import org.http4s.server.blaze.BlazeBuilder
 
 import scalaz._
 import Scalaz._
+import scalaz.concurrent.Task
 import scalaz.effect.IO
 
 object WebServerLauncher extends App with LogInitialization {
   // Initialize the log and exit if it fails
   configLog.run.onException(IO(sys.exit(1))).unsafePerformIO()
+
+  // Initialize logger after the configuration
+  val Log = Logger.getLogger(WebServerLauncher.getClass.getSimpleName)
+
+  // Attempt to get the file or throw an exception if not possible
+  def configurationFile: File = new File(new File(baseDir, "conf"), "app.cfg")
+
+  // Read the config, first attempt the file or default to the classpath file
+  val config: Task[Config] = knobs.loadImmutable(
+    knobs.Optional(FileResource(configurationFile)) :: Required(ClassPathResource("app.cfg")) :: Nil)
 
   val logger = Logger.getLogger(getClass.getName)
 
