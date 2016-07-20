4cd83af4582bbfb39993d41071ee774a63662c4f
Danny Navarro <j@dannynavarro.net>
2016-07-20 16:39:21 -0400

    Use RWST monad to handle logging, changing state and access the event channel
    Now Pause events are handled as well. WIP: Print still doesn't work so this is
    actually working.

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/sequence.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/sequence.scala
@@ -1,116 +1,160 @@
 package edu.gemini.seqexec.engine
 
+import scala.io.StdIn.readChar
+import scala.concurrent.Channel
+
 import scalaz._
 import Scalaz._
 import scalaz.concurrent.Task
-import scala.concurrent.Channel
 
 
 object Engine {
 
-  type Sequence = List[Step]
+  type Sequence = List[List[Action]]
 
   type Action = Task[Result]
 
+  sealed trait Status
+  case object Running extends Status
+  case object Waiting extends Status
+
+  type SeqStatus = (Sequence, Status)
+
   sealed trait Result
-  case object Done  extends Result
+  case object Done extends Result
   case object Error extends Result
 
-  case class Step(t: TcsConfig, i: InstConfig, o: Observation)
-
-  sealed trait HasAction
-  case class TcsConfig(a:Action) extends HasAction
-  case class InstConfig(a:Action) extends HasAction
-  case class Observation(a:Action) extends HasAction
-
-  def action(ha:HasAction): Action = ha match {
-    case TcsConfig(a:Action) => a
-    case InstConfig(a:Action) => a
-    case Observation(a:Action) => a
-  }
-
   sealed trait Event
-  case object Configured extends Event
-  case object Observed   extends Event
-  case object Completed  extends Event
+  case object Start extends Event
+  case object Pause extends Event
+  case object Completed extends Event  // Action failed
+  case object Failed extends Event // Action failed
+  case object Synced extends Event // Parallel actions completed
+  case object Finished extends Event
 
-  def execute(chan: Channel[Event], seq: Sequence): Task[Unit] = {
-    def step(s:Step): Task[Unit] = s match {
-      case Step(tcs: TcsConfig, inst: InstConfig, obsv: Observation) => for {
-        r <- concurrently(action(tcs), action(inst))
-        _ <- Task { if (r.equals((Done, Done))) { chan.write(Configured) } }
-        obr <- action(obsv)
-        } yield chan.write(Completed)
-      }
-    seq.traverse_(step)
-  }
+  def configureTcs: Action  = for
+    { _ <- Task { println("Start TCS configuration") }
+      _ <- Task { Thread.sleep(2000) }
+      _ <- Task { println ("Complete TCS configuration") }
+    } yield Done
 
-  sealed trait Status
-  case object Idle extends Status
-  case object Running extends Status
-  case object Failed extends Status
+  def configureInst : Action  = for
+    { _ <- Task { println("Start Instrument configuration") }
+      _ <- Task { Thread.sleep(2000) }
+      _ <- Task { println ("Complete Instrument configuration for") }
+    } yield Done
 
-  def handle (chan: Channel[Event]): Task[Unit] = {
-    def go (s:Status): Task[Unit] = s match {
-      case Idle => Task {
-        println("Output: Starting sequence")
-        go(Running)
-      }
-      case Failed => sys.error("Unimplemented")
-      case Running => for {
-        ev <- Task { chan.read }
-        _ <- ev match {
-          case Configured => Task {
-            println("Output: TCS and Instrument configured")
-            go(Running)
-          }
-          case Observed => Task {
-            println("Output: Observation completed")
-            go(Running)
-          }
-          case Completed  => Task {
-            println("Output: Sequence completed")
-          }
+  def observe : Action  = for
+    { _ <- Task { println("Start observation") }
+      _ <- Task { Thread.sleep(2000) }
+      _ <- Task { println ("Complete observation") }
+    } yield Done
+
+  def input[A](chan: Channel[Event]): Task[A] =
+    Bind[Task].forever(
+      Task { readChar() } >>= {
+        (c: Char) => c match {
+           case 'p' => Task { chan.write(Pause) }
+           case 's' => Task { chan.write(Start) }
+           case _   => Task(Unit)
+         }
         }
-      } yield Task(Unit)
-    }
-    go(Idle)
-  }
+    )
 
-  val sequence1 = {
-    List(
-      Step(
-        (TcsConfig
-           (for
-            { _ <- Task { println("Start TCS configuration for Step 1") }
-              _ <- Task { Thread.sleep(2000) }
-              _ <- Task { println ("Complete TCS configuration for Step 1") }
-            } yield Done
-           )
-        ),
-        (InstConfig
-           (for
-            { _ <- Task { println("Start instrument configuration for Step 1") }
-              _ <- Task { Thread.sleep(2000) }
-              _ <- Task { println("Complete instrument configuration for Step 1") }
-            } yield Done
-           )
-        ),
-        (Observation
-           (for
-            { _ <- Task { println("Start observation for Step 1") }
-              _ <- Task { Thread.sleep(5000) }
-              _ <- Task { println("Complete observation for Step 1") }
-            } yield Done
-           )
+  type Telescope[A] = ReaderWriterStateT[Task, Channel[Event], List[String], SeqStatus, A]
+
+  def handler[R]: Telescope[R] =
+    Bind[Telescope].forever(
+      receive >>= {
+        (ev: Event) => ev match {
+          case Start => log("Output: Started") *> switch(Running) *> execute
+          case Pause => log("Output: Paused") *> switch(Waiting)
+          case Completed => log("Output: Action completed")
+          case Failed => log("Output: Action failed")
+          case Synced => log("Output: Parallel actions completed") *> execute
+          case Finished => log("Output: Finished") *> switch(Waiting)
+        }
+      }
+    )
+
+  def log(msg:String): Telescope[Unit] =
+    MonadTell[Telescope, List[String]].tell(List(msg))
+
+  val receive: Telescope[Event] = for {
+    chan <- channel
+  } yield chan.read
+
+  def channel: Telescope[Channel[Event]] =
+    MonadReader[Telescope, Channel[Event]].ask
+
+  def switch(st: Status): Telescope[Unit] =
+    MonadState[Telescope, SeqStatus].modify(
+      (ss: SeqStatus) => ss.map(_ => st)
+    )
+
+  val status: Telescope[Status] =
+    MonadState[Telescope, SeqStatus].gets(_._2)
+
+  val execute: Telescope[Unit] = {
+    def exe(chan: Channel[Event], action:Action): Task[Unit] =
+      action >>= {
+        (r: Result) => r match {
+          case Done  => Task { chan.write(Completed) }
+          case Error => Task { chan.write(Failed) }
+        }
+      }
+
+    val go: Telescope[Unit] = for {
+      actions <- pop
+      chan <- MonadReader[Telescope, Channel[Event]].ask
+      _ <- Applicative[Telescope].pure(
+        Nondeterminism[Task].gatherUnordered(
+          actions.map(
+            (a: Action) => exe(chan, a)
+          )
         )
       )
+    } yield send(Synced)
+
+    status >>= {
+      (st: Status) => st match {
+        case Running => go
+        case Waiting => Applicative[Telescope].pure(Unit)
+      }
+    }
+  }
+
+  def send(ev: Event): Telescope[Unit] = for {
+    chan <- channel
+  } yield Applicative[Telescope].pure(
+    Task { chan.write(ev) }
+  )
+
+  def pop: Telescope[List[Action]] =
+    MonadState[Telescope, SeqStatus].get >>= {
+      (ss) => {
+         val (seq, st) = ss
+         seq match {
+           case (x :: xs) => MonadState[Telescope, SeqStatus].put((xs,st)) *> Applicative[Telescope].pure(x)
+           case _ => send(Finished) *> Applicative[Telescope].pure(List())
+         }
+      }
+    }
+
+  val sequence0: Sequence =
+    List(
+      List(configureTcs, configureInst),
+      List(observe),
+      List(configureTcs, configureInst),
+      List(observe)
     )
-}
+
   def main(args: Array[String]): Unit = {
     val chan = new Channel[Event]
-    concurrently(execute(chan, sequence1), handle(chan)).unsafePerformSync
+    /// XXX: Handle printing
+    val ((output, _), _) = Nondeterminism[Task].both(
+      handler.exec(chan, (sequence0, Waiting)),
+      input(chan)
+    ).unsafePerformSync
   }
-  private def concurrently[A, B](a: Task[A], b: Task[B]): Task[(A, B)] =
-    Nondeterminism[Task].both(a, b)
 }
