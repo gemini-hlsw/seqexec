47000c6e3fb5abb270e26b9e946ec0cc4219f546
Danny Navarro <j@dannynavarro.net>
2016-07-29 15:26:33 -0400

    Differentiate between user input events and system events

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/Handler.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/Handler.scala
@@ -11,19 +11,30 @@ object Handler {
   /**
     * Main logical thread to handle events and produce output.
     */
-  def handler[R](chan: Channel[Event]): Telescope[R] =
+  def handler[R](chan: Channel[Event]): Telescope[R] = {
+
+    def handleUserEvent(ue: UserEvent): Telescope[Unit] = ue match {
+      case Start => log("Output: Started") *> switch(Running) *> run(chan)
+      case Pause => log("Output: Paused") *> switch(Waiting)
+      case AddStep(ste) => log("Output: Adding Step") *> add(ste)
+    }
+
+    def handleSystemEvent(se: SystemEvent) = se match {
+      case Completed => log("Output: Action completed")
+      case Failed => log("Output: Action failed")
+      case Synced => log("Output: Parallel actions completed") *> tail *> run(chan)
+      case SyncFailed => log("Output: Step failed. Repeating...") *> run(chan)
+      case Finished => log("Output: Finished") *> switch(Waiting)
+    }
+
     (receive(chan) >>= {
-      (ev: Event) => ev match {
-        case Start => log("Output: Started") *> switch(Running) *> run(chan)
-        case Pause => log("Output: Paused") *> switch(Waiting)
-        case Completed => log("Output: Action completed")
-        case Failed => log("Output: Action failed")
-        case Synced => log("Output: Parallel actions completed") *> tail *> run(chan)
-        case SyncFailed => log("Output: Step failed. Repeating...") *> run(chan)
-        case Finished => log("Output: Finished") *> switch(Waiting)
-        case AddStep(ste) => log("Output: Adding Step") *> add(ste)
-      }
+       (ev: Event) => ev match {
+         case EventUser(ue) => handleUserEvent(ue)
+         case EventSystem(se) => handleSystemEvent(se)
+       }
+     }
     // XXX: `forever`, for some reason, keeps looping over the same message even
     // if only sent once.
-     }) >> handler(chan)
+    ) >> handler(chan)
+  }
 }
