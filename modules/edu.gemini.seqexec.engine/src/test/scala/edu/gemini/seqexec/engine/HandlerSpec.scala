624639c477ce020f786fe18532a00c5223a6df10
Danny Navarro <j@dannynavarro.net>
2016-08-18 11:47:46 -0300

    Change terminology to better match existing models
    This in preparation for model consolidation across the whole project.

--- modules/edu.gemini.seqexec.engine/src/test/scala/edu/gemini/seqexec/engine/HandlerSpec.scala
+++ modules/edu.gemini.seqexec.engine/src/test/scala/edu/gemini/seqexec/engine/HandlerSpec.scala
@@ -2,10 +2,10 @@ package edu.gemini.seqexec.engine
 
 import scala.collection.immutable.IntMap
 
+import Result._
 import Engine._
 import Event._
 import Handler._
-import State._
 import org.scalatest.FlatSpec
 import scalaz._
 import Scalaz._
@@ -19,7 +19,7 @@ class HandlerSpec extends FlatSpec {
     * Emulates TCS configuration in the real world.
     *
     */
-  val configureTcs: Action  = for {
+  val configureTcs: Execution.Action  = for {
     _ <- Task(println("System: Start TCS configuration"))
     _ <- Task(Thread.sleep(2000))
     _ <- Task(println ("System: Complete TCS configuration"))
@@ -29,7 +29,7 @@ class HandlerSpec extends FlatSpec {
     * Emulates Instrument configuration in the real world.
     *
     */
-  val configureInst: Action  = for {
+  val configureInst: Execution.Action  = for {
     _ <- Task(println("System: Start Instrument configuration"))
     _ <- Task(Thread.sleep(2000))
     _ <- Task(println("System: Complete Instrument configuration"))
@@ -39,20 +39,20 @@ class HandlerSpec extends FlatSpec {
     * Emulates an observation in the real world.
     *
     */
-  val observe: Action  = for {
+  val observe: Execution.Action  = for {
     _ <- Task(println("System: Start observation"))
     _ <- Task(Thread.sleep(2000))
     _ <- Task(println ("System: Complete observation"))
   } yield OK
 
-  val faulty: Action  = for {
+  val faulty: Execution.Action  = for {
     _ <- Task(println("System: Start observation"))
     _ <- Task(Thread.sleep(1000))
     _ <- Task(println ("System: Complete observation"))
   } yield OK
 
-  val seqstatus1: SeqStatus = SeqStatus(
-    Sequence(
+  val qs1: QueueStatus = QueueStatus(
+    Queue(
       List(),
       IntMap(),
       List(
@@ -61,29 +61,29 @@ class HandlerSpec extends FlatSpec {
         List(configureTcs, configureInst),
         List(observe)
       )),
-    Waiting)
+    Status.Waiting)
 
-  val emptySeqStatus: SeqStatus =
-    SeqStatus(Sequence(List(), IntMap(), List()), Waiting)
+  val emptySeqStatus: QueueStatus =
+    QueueStatus(Queue(List(), IntMap(), List()), Status.Waiting)
 
   val queue = async.boundedQueue[Event](10)
 
   // Send an event and obtain a `SeqStatus`. This takes care of terminating the
   // process.
-  def request(ev: Event): Task[SeqStatus] =
+  def request(ev: Event): Task[QueueStatus] =
     queue.enqueueOne(start) *>
       (handler(queue).take(1).run.exec(emptySeqStatus))
 
   it should "be in running Status after a Start event" in {
     val result = request(start).unsafePerformSync.status
-    assert(result === Running)
+    assert(result === Status.Running)
   }
 
   it should "0 steps pending after 4 steps have been processed for seqstatus1" in {
     val result = (queue.enqueueOne(start) *>
       // TODO: Don't hardcode the number of steps to let through.
-      (handler(queue).take(7).run.exec(seqstatus1))).unsafePerformSync
-    assert(result.sequence.pending.length == 0)
+      (handler(queue).take(7).run.exec(qs1))).unsafePerformSync
+    assert(result.queue.pending.length == 0)
   }
 
   // This is here for visual inspection. The states should be coupled with a //
@@ -91,13 +91,13 @@ class HandlerSpec extends FlatSpec {
   it should "0 steps pending after pause/start events" in {
     val result =
       (queue.enqueueAll(List(start, poll, pause, poll, start, poll)) *>
-         (handler(queue).take(12).run.exec(seqstatus1))).unsafePerformSync
-    assert(result.sequence.pending.length == 0)
+         (handler(queue).take(12).run.exec(qs1))).unsafePerformSync
+    assert(result.queue.pending.length == 0)
   }
   it should "finish raising a Terminated exception after an Exit event" in {
     intercept[Cause.Terminated](Nondeterminism[Task].both(
       (queue.enqueueOne(start) *> queue.enqueueOne(exit)),
-      (handler(queue).run.exec(seqstatus1))
+      (handler(queue).run.exec(qs1))
       ).unsafePerformSync
     )
   }
