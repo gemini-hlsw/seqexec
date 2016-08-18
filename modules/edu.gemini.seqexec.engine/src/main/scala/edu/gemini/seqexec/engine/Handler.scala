f89697e463d291b9bd90cfa4616cd996c9cc4113
Danny Navarro <j@dannynavarro.net>
2016-08-18 15:51:38 -0300

    Make Engine a package object
    Also rename Seqexec -> Engine

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/Handler.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/Handler.scala
@@ -1,7 +1,6 @@
 package edu.gemini.seqexec.engine
 
 import Event._
-import Engine._
 import scalaz.Scalaz._
 import scalaz.stream.Process
 
@@ -10,9 +9,9 @@ object Handler {
   /**
     * Main logical thread to handle events and produce output.
     */
-  def handler(q: EventQueue): Process[Seqexec, QueueStatus] = {
+  def handler(q: EventQueue): Process[Engine, QueueStatus] = {
 
-    def handleUserEvent(ue: UserEvent): Seqexec[QueueStatus] = ue match {
+    def handleUserEvent(ue: UserEvent): Engine[QueueStatus] = ue match {
       case Start => log("Output: Started") *> switch(q)(Status.Running)
       case Pause => log("Output: Paused") *> switch(q)(Status.Waiting)
       case AddExecution(pend) => log("Output: Adding Pending Execution") *> add(pend)
@@ -20,7 +19,7 @@ object Handler {
       case Exit => log("Bye") *> close(q)
     }
 
-    def handleSystemEvent(se: SystemEvent): Seqexec[QueueStatus] = se match {
+    def handleSystemEvent(se: SystemEvent): Engine[QueueStatus] = se match {
       case (Completed(i)) => log("Output: Action completed") *> complete(q)(i)
       case (Failed(i)) => log("Output: Action failed") *> fail(q)(i)
       case Finished => log("Output: Finished")
