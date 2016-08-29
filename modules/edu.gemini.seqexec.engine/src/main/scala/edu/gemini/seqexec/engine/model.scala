043d9b92f3aa011829ce43bc9d54ef4e567aa500
Danny Navarro <j@dannynavarro.net>
2016-08-29 16:09:17 -0300

    Deepen model structure
    Roughly: `QueueStatus -> Queue -> Sequences -> Steps -> Executions`
    
    Also use partial lens to handle deep structure modification.

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/model.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/model.scala
@@ -3,7 +3,7 @@ package edu.gemini.seqexec.engine
 import scala.collection.immutable.IntMap
 
 import scalaz._
-import scalaz.Scalaz._
+import Scalaz._
 import scalaz.concurrent.Task
 
 /**
@@ -16,13 +16,13 @@ case class QueueStatus(queue: Queue, status: Status)
 object QueueStatus {
 
   /**
-   * Lens to manipulate the `Sequence` under `SeqStatus`
+   * Lens to manipulate the `Sequence` under `Engine`
    */
   val queue: QueueStatus @> Queue =
     Lens.lensu((ss, q1) => ss.copy(queue = q1), _.queue)
 
   /**
-   * Lens to manipulate the `Status` under `SeqStatus`
+   * Lens to manipulate the `Status` under `Engine`
    */
   val status: QueueStatus @> Status =
     Lens.lensu((ss, st1) => ss.copy(status = st1), _.status)
@@ -36,7 +36,7 @@ object QueueStatus {
     * Lens for current actions. These are the actions being executed in
     * parallel.
     */
-  val current: QueueStatus @> Execution.Current = queue >=> Queue.current
+  val current: QueueStatus @> Queue.Current = queue >=> Queue.current
 
   /**
     * Lens for completed Steps.
@@ -49,20 +49,34 @@ object QueueStatus {
    */
   def prime(ss0: QueueStatus): Option[QueueStatus] = {
 
-    def remove(l: Queue.Pending): Queue.Pending = tailOption(l).getOrElse(List())
+    def peek(qs: QueueStatus): Execution.Pending =
+      // Sequence
+      (qs.queue.pending.headOption >>=
+        // Step
+        (_.headOption) >>=
+        // Execution
+        (_.headOption)
+      ).getOrElse(List())
+
+    def remove(qs: QueueStatus): QueueStatus =
+      (QueueStatus.pending.partial >=>
+         PLens.listHeadPLens[Sequence.Pending] >=>
+           PLens.listHeadPLens[Step.Pending] >=>
+         PLens.listHeadPLens[Execution.Pending]
+      ).mod(_.tailOption.getOrElse(List()), qs)
 
     def toIntMap[A](l: List[A]): IntMap[A] =
       IntMap(l.zipWithIndex.map(s => (s._2, s._1)).toSeq: _*)
 
     if (QueueStatus.current.get(ss0).isEmpty) {
-      // Peek pending step
-      val h = ss0.queue.pending.headOption.getOrElse(List())
       // Convert pending step to current step
-      val ss1 = QueueStatus.current.set(ss0, toIntMap(h))
+      val ss1 = QueueStatus.current.set(ss0, toIntMap(peek(ss0)))
       // Remove pending step
-      Some(QueueStatus.pending.mod(remove, ss1))
+      Some (remove(ss1))
+      // Some(QueueStatus.pending.mod(remove, ss1))
     } else { None }
   }
+
 
   /*
    * Given the index of a completed Action in the current Step, it moves such
@@ -71,14 +85,12 @@ object QueueStatus {
    */
   def shift(i: Int)(ss0: QueueStatus): QueueStatus = {
 
-    // TODO: Stock Lens for List?
     // Add an index to the list of completed steps.
     def add(steps: Queue.Done): Queue.Done =
-      steps match {
-        case Nil => List(List(i))
-        // Only the head Step is considered.
-        case (x :: xs) => (i :: x) :: xs
-      }
+      (PLens.listHeadPLens[Sequence.Done] >=>
+       PLens.listHeadPLens[Step.Done] >=>
+       PLens.listHeadPLens[Execution.Done]
+      ).mod(i :: _, steps)
 
     // Remove action from current step
     val ss1 = QueueStatus.current.mod(_ - i, ss0)
@@ -91,36 +103,27 @@ object QueueStatus {
   * A List of `Step`s meant to be run sequentially.
   */
 case class Queue(
-  done: List[Execution.Done],
-  current: Execution.Current,
-  pending: List[Execution.Pending]
+  pending: Queue.Pending,
+  current: Queue.Current,
+  done: Queue.Done
 )
 
 object Queue {
 
-  type Pending = List[Execution.Pending]
+  type Pending = List[Sequence.Pending]
 
-  type Done = List[Execution.Done]
+  type Current = Sequence.Current
 
-  /**
-    * Lens for the remaining Steps.
-    */
+  type Done = List[Sequence.Done]
+
   val pending: Queue @> Queue.Pending =
-    Lens.lensu((q, newSteps) => q.copy(pending = newSteps), _.pending)
+    Lens.lensu((q, qp) => q.copy(pending = qp), _.pending)
 
-  /**
-    * Lens for current actions. These are the actions being executed in
-    * parallel.
-    */
-  val current: Queue @> Execution.Current =
-    Lens.lensu((q, newStep) => q.copy(current = newStep), _.current)
+  val current: Queue @> Queue.Current =
+    Lens.lensu((q, qc) => q.copy(current = qc), _.current)
 
-  /**
-    * Lens for completed Steps.
-    */
   val done: Queue @> Queue.Done =
-    Lens.lensu((q, newSteps) => q.copy(done = newSteps), _.done)
-
+    Lens.lensu((q, qd) => q.copy(done = qd), _.done)
 }
 
 /**
@@ -133,29 +136,77 @@ object Status {
   case object Waiting extends Status
 }
 
+case class Sequence(
+  pending: Sequence.Pending,
+  current: Sequence.Current,
+  done: Sequence.Done
+)
+
+object Sequence {
+
+  type Pending = List[Step.Pending]
+
+  type Current = Step.Current
+
+  type Done = List[Step.Done]
+
+  val pending: Sequence @> Sequence.Pending =
+    Lens.lensu((s, ns) => s.copy(pending = ns), _.pending)
+
+  val current: Sequence @> Sequence.Current =
+    Lens.lensu((s, ns) => s.copy(current = ns), _.current)
+
+  val done: Sequence @> Sequence.Done =
+    Lens.lensu((s, ns) => s.copy(done = ns), _.done)
+}
+
+case class Step(
+  pending: Step.Pending,
+  current: Step.Current,
+  done: Step.Done
+)
+
+object Step {
+
+  type Pending = List[Execution.Pending]
+
+  type Current = Execution.Current
+
+  type Done = List[Execution.Done]
+
+  val pending: Step @> Step.Pending =
+    Lens.lensu((s, ns) => s.copy(pending = ns), _.pending)
+
+  val current: Step @> Step.Current =
+    Lens.lensu((s, ns) => s.copy(current = ns), _.current)
+
+  val done: Step @> Step.Done =
+    Lens.lensu((s, ns) => s.copy(done = ns), _.done)
+}
+
 object Execution {
+  /**
+    * This represents something to be done in the underlying systems.
+    */
+  type Action = Task[Result]
+
+  /**
+    * Actions with static indexing. This is meant to be used for the transition
+    * of parallel actions from ongoing to completed. An ordinary list won't keep
+    * the original index as actions are removed.
+    */
+  type Current = IntMap[Action]
+
   /**
    *  A list of actions to be run in parallel.
    */
   type Pending = List[Action]
 
   /**
-   * Actions with static indexing. This is meant to be used for the transition
-   * of parallel actions from ongoing to completed. An ordinary list won't keep
-   * the original index as actions are removed.
-   */
-  type Current = IntMap[Action]
-
-  /**
    * A list of successfully completed actions represented with an index. This
    * index can be used to backtrack the correspondent original action.
    */
   type Done = List[Int]
-
-  /**
-    * This represents something to be done in the underlying systems.
-    */
-  type Action = Task[Result]
 }
 
 /**
