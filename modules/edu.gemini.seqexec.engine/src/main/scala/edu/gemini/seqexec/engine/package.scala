2419ce8695a3832411a833559be9e3604dafdadd
Danny Navarro <j@dannynavarro.net>
2016-08-30 15:15:06 -0300

    Update documentation

--- modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/package.scala
+++ modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/package.scala
@@ -10,17 +10,25 @@ import scalaz.stream.async.mutable.{Queue => SQueue}
 
 package object engine {
 
-  type EventQueue = SQueue[Event]
   /**
-    * Type constructor where all all side effects related to the Engine are
-    * managed.
+    * This represents an actual real-world action to be done in the underlying
+    * systems.
+    */
+  type Action = Task[Result]
+
+  // Avoid name class with the proper Seqexec `Queue`
+  type EventQueue = SQueue[Event]
+
+  /**
+    * Type constructor where all Seqexec side effect are managed.
     */
   type Engine[A] = EngineStateT[Task, A]
   // Helper alias to facilitate lifting.
   type EngineStateT[M[_], A] = StateT[M, QueueStatus, A]
 
   /**
-    * Return `SeqStatus` while changing `Status` within the `Engine` monad.
+    * Changes the `Status` and returns the new `QueueStatus`
+    *
     * This also takes care of initiating the execution when transitioning to
     * `Running` status.
     */
@@ -31,24 +39,71 @@ package object engine {
       ) *> get
 
   /**
-    * Transfer the next pending `Step` to current `Step` when the current `Step`
-    * is empty. If the current Step is not empty, `None` without any change in
-    * the `SeqStatus`.
+    * Given the index of the completed `Action` in the current `Execution`, it
+    * marks the `Action` as completed and returns the new `QueueStatus`.
+    *
+    * When the index doesn't exit it does nothing.
     */
-  private val prime: Engine[Option[Execution.Current]] =
-    gets(QueueStatus.prime(_)) >>= {
-      case Some(qs) => put(qs) *> pure(Some(qs.queue.current))
-      case None => pure(None)
-    }
+  def complete(q: EventQueue)(i: Int): Engine[QueueStatus] =
+    modify(QueueStatus.shift(i)(_)) *> get
 
   /**
-    * Checks the `Status` is `Running` and executes all actions in a current
-    * `Step` in parallel. It also updates the `SeqStatus` as needed.
+    * For now it only changes the `Status` to `Paused` and returns the new
+    * `QueueStatus`. In the future this function should handle the failed
+    * action.
+    */
+  def fail(q: EventQueue)(i: Int): Engine[QueueStatus] = switch(q)(Status.Waiting)
+
+  /**
+    * Launches the next `Execution` when the current `Execution` is empty while
+    * returning the `QueueStatus` just before the next execution starts. This is
+    * mainly meant to be used to handle `Executed` events.
+    */
+  def next(q: EventQueue): Engine[QueueStatus] =
+    (prime >>= (_.fold(unit)(step(q)(_)))) *> get
+
+  /**
+    * Adds an `Execution` to the beginning of the `QueueStatus` while returning
+    * the `QueueStatus`.
+    */
+  // TODO: Change this to the end or insert by index. For that List -> Vector in
+  // `QueueStatus`
+  def add(pend: Execution.Pending): Engine[QueueStatus] = {
+    val lens = QueueStatus.pending.partial >=>
+      PLens.listHeadPLens[Sequence.Pending] >=>
+      PLens.listHeadPLens[Step.Pending]
+    modify(lens.mod((pend :: _), _)) *> get
+  }
+
+  /**
+    * Ask for the current Engine `Status`.
+    */
+  val status: Engine[Status] = gets(_.status)
+
+  /**
+    * Log something and return the `QueueStatus`
+    */
+  // XXX: Proper Java logging
+  def log(msg: String): Engine[QueueStatus] = pure(println(msg)) *> get
+
+  /** Terminates the `Engine` returning the final `QueueStatus`.
+    */
+  def close(queue: EventQueue): Engine[QueueStatus] =
+    queue.close.liftM[EngineStateT] *> get
+
+  /**
+    * Enqueue `Event` in the Engine.
+    */
+  private def send(q: EventQueue)(ev: Event): Engine[Unit] = pure(q.enqueueOne(ev))
+
+  /**
+    * Checks the `Status` is `Running` and executes all actions in the current
+    * `Execution` in parallel. It also updates the `QueueStatus` as needed.
     */
   private def step(q: EventQueue)(actions: Execution.Current): Engine[Unit] = {
 
     // Send the expected event when action is executed
-    def execute(t: (Int, Execution.Action)): Task[Unit] = {
+    def execute(t: (Int, Action)): Task[Unit] = {
       val (i, action) = t
       action >>= {
         case Result.OK => q.enqueueOne(completed(i))
@@ -65,60 +120,23 @@ package object engine {
     }
   }
 
-  def next(q: EventQueue): Engine[QueueStatus] =
-    (prime >>= (_.fold(unit)(step(q)(_)))) *> get
-
   /**
-    * Given the index in the current `Step` of the completed action, it
-    * transfers such action to the current done Step. If the current Step
-    * becomes empty it takes care of priming the next Step.
+    * Promote the next pending `Execution` to current when the current
+    * `Execution` is empty. If the current `Execution` is not empty it does
+    * nothing.
     */
-  def complete(q: EventQueue)(i: Int): Engine[QueueStatus] =
-    modify(QueueStatus.shift(i)(_)) *> get
-
-  // For now stop the seqexec when an action fails.
-  def fail(q: EventQueue)(i: Int): Engine[QueueStatus] = switch(q)(Status.Waiting)
-
-  /**
-    * Ask for the current `Status` within the `Engine` monad.
-    */
-  val status: Engine[Status] = gets(_.status)
-
-  /**
-    * Send an event within the `Engine` monad.
-    */
-  private def send(q: EventQueue)(ev: Event): Engine[Unit] = pure(q.enqueueOne(ev))
-
-  /**
-    * Log within the `Engine` monad as a side effect while returning the
-    * `SeqStatus`.
-    */
-  // XXX: log4j?
-  def log(msg: String): Engine[QueueStatus] = pure(println(msg)) *> get
-
-  /**
-    * Add an `Execution` to the beginning of the Queue while returning the
-    * `SeqStatus`.
-    */
-  def add(pend: Execution.Pending): Engine[QueueStatus] = {
-    val lens = QueueStatus.pending.partial >=>
-       PLens.listHeadPLens[Sequence.Pending] >=>
-       PLens.listHeadPLens[Step.Pending]
-    modify(lens.mod((pend :: _), _)) *> get
+  private val prime: Engine[Option[Execution.Current]] =
+    gets(QueueStatus.prime(_)) >>= {
+      case Some(qs) => put(qs) *> pure(Some(qs.queue.current))
+      case None => pure(None)
     }
-  /** Terminates the queue while returning the final `SeqStatus`
-    */
-  def close(queue: EventQueue): Engine[QueueStatus] =
-    queue.close.liftM[EngineStateT] *> get
 
-  // Functions to deal with type bureaucracy
+  // Functions to facilitate type bureaucracy
 
   /**
     * This creates a `Event` Process with `Engine` as effect.
     */
   def receive(queue: EventQueue): Process[Engine, Event] = hoistEngine(queue.dequeue)
-
-  // Type bureaucracy
 
   private def pure[A](a: A): Engine[A] = Applicative[Engine].pure(a)
 
