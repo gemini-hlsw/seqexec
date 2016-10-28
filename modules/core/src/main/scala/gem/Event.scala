df44780e397541bf5a466599f9a96ad29eb13c1f
swalker2m <swalker2m@mac.com>
2016-10-28 10:08:58 -0300

    Remove sequence id concept.

--- modules/core/src/main/scala/gem/Event.scala
+++ modules/core/src/main/scala/gem/Event.scala
@@ -4,44 +4,44 @@ import java.time.Instant
 
 sealed trait Event {
   def timestamp: Instant
-  def sid: Sequence.Id
+  def oid: Observation.Id
 }
 
 object Event {
 
-  case class StartSlew(       timestamp: Instant, sid: Sequence.Id) extends Event
-  case class EndSlew(         timestamp: Instant, sid: Sequence.Id) extends Event
+  case class StartSlew(       timestamp: Instant, oid: Observation.Id) extends Event
+  case class EndSlew(         timestamp: Instant, oid: Observation.Id) extends Event
 
-  case class StartVisit(      timestamp: Instant, sid: Sequence.Id) extends Event
-  case class EndVisit(        timestamp: Instant, sid: Sequence.Id) extends Event
+  case class StartVisit(      timestamp: Instant, oid: Observation.Id) extends Event
+  case class EndVisit(        timestamp: Instant, oid: Observation.Id) extends Event
 
-  case class StartSequence(   timestamp: Instant, sid: Sequence.Id) extends Event
-  case class EndSequence(     timestamp: Instant, sid: Sequence.Id) extends Event
+  case class StartSequence(   timestamp: Instant, oid: Observation.Id) extends Event
+  case class EndSequence(     timestamp: Instant, oid: Observation.Id) extends Event
 
-  case class PauseObserve(    timestamp: Instant, sid: Sequence.Id) extends Event
-  case class ContinueObserve( timestamp: Instant, sid: Sequence.Id) extends Event
-  case class AbortObserve(    timestamp: Instant, sid: Sequence.Id) extends Event
-  case class StopObserve(     timestamp: Instant, sid: Sequence.Id) extends Event
+  case class PauseObserve(    timestamp: Instant, oid: Observation.Id) extends Event
+  case class ContinueObserve( timestamp: Instant, oid: Observation.Id) extends Event
+  case class AbortObserve(    timestamp: Instant, oid: Observation.Id) extends Event
+  case class StopObserve(     timestamp: Instant, oid: Observation.Id) extends Event
 
-  case class StartIntegration(timestamp: Instant, sid: Sequence.Id, step: Int) extends Event
-  case class EndIntegration(  timestamp: Instant, sid: Sequence.Id, step: Int) extends Event
+  case class StartIntegration(timestamp: Instant, oid: Observation.Id, step: Int) extends Event
+  case class EndIntegration(  timestamp: Instant, oid: Observation.Id, step: Int) extends Event
 
 
-  def startSlew(t: Instant, s: Sequence.Id): Event       = StartSlew(t, s)
-  def endSlew(t: Instant, s: Sequence.Id): Event         = EndSlew(t, s)
+  def startSlew(t: Instant, o: Observation.Id): Event       = StartSlew(t, o)
+  def endSlew(t: Instant, o: Observation.Id): Event         = EndSlew(t, o)
 
-  def startVisit(t: Instant, s: Sequence.Id): Event      = StartVisit(t, s)
-  def endVisit(t: Instant, s: Sequence.Id): Event        = EndVisit(t, s)
+  def startVisit(t: Instant, o: Observation.Id): Event      = StartVisit(t, o)
+  def endVisit(t: Instant, o: Observation.Id): Event        = EndVisit(t, o)
 
-  def startSequence(t: Instant, s: Sequence.Id): Event   = StartSequence(t, s)
-  def endSequence(t: Instant, s: Sequence.Id): Event     = EndSequence(t, s)
+  def startSequence(t: Instant, o: Observation.Id): Event   = StartSequence(t, o)
+  def endSequence(t: Instant, o: Observation.Id): Event     = EndSequence(t, o)
 
-  def pauseObserve(t: Instant, s: Sequence.Id): Event    = PauseObserve(t, s)
-  def continueObserve(t: Instant, s: Sequence.Id): Event = ContinueObserve(t, s)
-  def abortObserve(t: Instant, s: Sequence.Id): Event    = AbortObserve(t, s)
-  def stopObserve(t: Instant, s: Sequence.Id): Event     = StopObserve(t, s)
+  def pauseObserve(t: Instant, o: Observation.Id): Event    = PauseObserve(t, o)
+  def continueObserve(t: Instant, o: Observation.Id): Event = ContinueObserve(t, o)
+  def abortObserve(t: Instant, o: Observation.Id): Event    = AbortObserve(t, o)
+  def stopObserve(t: Instant, o: Observation.Id): Event     = StopObserve(t, o)
 
-  def startIntegration(t: Instant, s: Sequence.Id, i: Int): Event = StartIntegration(t, s, i)
-  def endIntegration(t: Instant, s: Sequence.Id, i: Int): Event   = EndIntegration(t, s, i)
+  def startIntegration(t: Instant, o: Observation.Id, i: Int): Event = StartIntegration(t, o, i)
+  def endIntegration(t: Instant, o: Observation.Id, i: Int): Event   = EndIntegration(t, o, i)
 
 }
