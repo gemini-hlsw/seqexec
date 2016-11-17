d4161c535b74eefbeb41d77629524d589a192fdf
swalker2m <swalker2m@mac.com>
2016-11-17 10:59:49 -0300

    Don't allow a lamp to be created from an invalid configuration

--- modules/core/src/main/scala/gem/config/GcalConfig.scala
+++ modules/core/src/main/scala/gem/config/GcalConfig.scala
@@ -20,13 +20,25 @@ object GcalConfig {
   type GcalArcs = OneAnd[ISet, GcalArc]
   type GcalLamp = GcalContinuum \/ GcalArcs
 
-  def mkLamp(continuum: Option[GcalContinuum], arcs: (GcalArc, Boolean)*): Option[GcalLamp] =
-    continuum.map(_.left[GcalArcs]).orElse(arcs.filter(_._2).unzip._1.toList match {
-      case h :: t => Some(OneAnd(h, ISet.fromList(t)).right[GcalContinuum])
-      case _      => None
-    })
+  def mkLamp(continuum: Option[GcalContinuum], arcs: (GcalArc, Boolean)*): Option[GcalLamp] = {
+    // Extract the arc lamps to include, if any.
+    val as = arcs.filter(_._2).unzip._1.toList
 
-  def unsafeMkLamp(co: Option[GcalContinuum], as: (GcalArc, Boolean)*): GcalLamp =
-    mkLamp(co, as: _*).getOrElse(sys.error("no Gcal continuum nor arc lamps"))
+    // Extract the continuum lamp, assuming there are no arcs.
+    val co = continuum.flatMap { c => as.isEmpty option c.left[GcalArcs] }
+
+    // Prepare the arc lamps, assuming there is no continuum.
+    val ao = as match {
+      case h :: t if continuum.isEmpty => Some(OneAnd(h, ISet.fromList(t)).right[GcalContinuum])
+      case _                           => None
+    }
+
+    co orElse ao
+  }
+
+  def unsafeMkLamp(continuum: Option[GcalContinuum], arcs: (GcalArc, Boolean)*): GcalLamp =
+    mkLamp(continuum, arcs: _*).getOrElse {
+      sys.error(s"misconfigured Gcal lamps: continuum=$continuum, arcs=[${arcs.filter(_._2).unzip._1.mkString(",")}]")
+    }
 }
 
