3502caa73e820a1c8248bff966eaca76b31d9ce4
Carlos Quiroz <cquiroz@gemini.edu>
2016-12-05 15:07:41 -0300

    SEQNG-58 Update to the latest ocs release

--- project/Settings.scala
+++ project/Settings.scala
@@ -61,7 +61,7 @@ object Settings {
     val jQuery         = "3.0.0"
     val semanticUI     = "2.2.2"
     val jQueryTerminal = "0.11.2"
-    val ocsVersion     = "2016102.1.1"
+    val ocsVersion     = "2017001.1.1"
   }
 
   /**
