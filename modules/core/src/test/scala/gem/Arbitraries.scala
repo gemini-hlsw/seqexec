a2280783b930b20ccb594fb843fc413a7d9ef8e0
swalker2m <swalker2m@mac.com>
2016-12-07 14:40:17 -0300

    Location refactor and find function.

--- modules/core/src/test/scala/gem/Arbitraries.scala
+++ modules/core/src/test/scala/gem/Arbitraries.scala
@@ -4,9 +4,25 @@ import org.scalacheck._
 import org.scalacheck.Gen._
 import org.scalacheck.Arbitrary._
 
+import scalaz._, Scalaz._
+
 trait Arbitraries {
 
+  implicit val arbLocationMiddle: Arbitrary[Location.Middle] =
+    Arbitrary {
+      for {
+        i  <- choose(Int.MinValue + 1, Int.MaxValue)
+        is <- arbitrary[List[Int]]
+      } yield Location.unsafeMiddle(i, is: _*)
+    }
+
   implicit val arbLocation: Arbitrary[Location] =
-    Arbitrary { arbitrary[List[Int]].map(Location.fromList) }
+    Arbitrary {
+      Gen.frequency[Location](
+        (1, Location.Beginning),
+        (8, arbitrary[Location.Middle]),
+        (1, Location.End)
+      )
+    }
 
 }
