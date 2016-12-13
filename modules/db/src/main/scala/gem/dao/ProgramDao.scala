d76a9a513f9580570090ab107e5572380f5a1a2a
swalker2m <swalker2m@mac.com>
2016-12-13 16:36:02 -0300

    Just use ProgramId.Daily.start time

--- modules/db/src/main/scala/gem/dao/ProgramDao.scala
+++ modules/db/src/main/scala/gem/dao/ProgramDao.scala
@@ -10,8 +10,6 @@ import edu.gemini.spModel.core.ProgramId._
 import doobie.imports._
 import doobie.contrib.postgresql.syntax._
 
-import java.util.{Calendar, GregorianCalendar}
-
 import scalaz._, Scalaz._
 
 object ProgramDao {
@@ -51,12 +49,7 @@ object ProgramDao {
                    ${pid.index})
     """.update.run
 
-  private def insertDailyProgramIdSlice(pid: ProgramId.Daily): ConnectionIO[Int] = {
-    val cal = new GregorianCalendar(pid.siteVal.timezone())
-    // Calendar month is 0-based, day starts at 2PM / 14:00 local time
-    cal.set(pid.year, pid.month - 1, pid.day, 14, 0, 0)
-    cal.set(Calendar.MILLISECOND, 0)
-
+  private def insertDailyProgramIdSlice(pid: ProgramId.Daily): ConnectionIO[Int] =
     sql"""
       INSERT INTO program (program_id,
                           site,
@@ -65,9 +58,8 @@ object ProgramDao {
             VALUES (${pid: Program.Id},
                     ${pid.siteVal.toString},
                     ${pid.ptypeVal.toString},
-                    ${cal.getTime})
+                    ${new java.util.Date(pid.start)})
     """.update.run
-  }
 
   private def insertArbitraryProgramIdSlice(pid: ProgramId.Arbitrary): ConnectionIO[Int] =
     pid.semester.traverse(SemesterDao.canonicalize) *>
