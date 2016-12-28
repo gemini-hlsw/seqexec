d6bcdb75fe3113c80d9294d5b30639e850f6e88c
Carlos Quiroz <cquiroz@gemini.edu>
2016-12-28 17:19:56 -0300

    SEQNG-90 Tests for websockets verifying the initial message with the user info #time 6h

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/test/scala/edu/gemini/seqexec/web/server/http4s/SeqexecUIApiRoutesSpec.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/test/scala/edu/gemini/seqexec/web/server/http4s/SeqexecUIApiRoutesSpec.scala
@@ -1,9 +1,11 @@
 package edu.gemini.seqexec.web.server.http4s
 
+import java.nio.ByteBuffer
+
 import edu.gemini.seqexec.web.server.security.{AuthenticationConfig, AuthenticationService, LDAPConfig}
 import edu.gemini.seqexec.engine.Event
 import edu.gemini.seqexec.model.Model.SeqexecEvent
-import edu.gemini.seqexec.model.{ModelBooPicklers, UserLoginRequest}
+import edu.gemini.seqexec.model.{ModelBooPicklers, UserDetails, UserLoginRequest}
 import edu.gemini.seqexec.server.SeqexecEngine
 import edu.gemini.seqexec.server.SeqexecEngine.Settings
 import org.http4s._
@@ -15,25 +17,29 @@ import squants.time._
 import boopickle.Default._
 
 import scalaz.stream.Process.emit
+import scalaz.stream.Process
 import scalaz.stream.async
 import java.nio.charset.StandardCharsets
 import java.time.{Instant, LocalDate}
 import java.time.temporal.ChronoUnit
 
+import edu.gemini.seqexec.model.Model.SeqexecEvent.ConnectionOpenEvent
+import org.http4s.websocket.WebsocketBits
 import org.scalatest.{FlatSpec, Matchers}
 
 import scalaz.OptionT
 import scalaz.concurrent.Task
+import scalaz.stream.async.mutable.{Queue, Topic}
 
 class SeqexecUIApiRoutesSpec extends FlatSpec with Matchers with UriFunctions with ModelBooPicklers with CaseInsensitiveStringSyntax {
   val config = AuthenticationConfig(devMode = true, Hours(8), "token", "abc", useSSL = false, LDAPConfig(Nil))
   val engine = SeqexecEngine(Settings("", LocalDate.now(), "", dhsSim = true, tcsSim = true, instSim = true, gcalSim = true))
   val authService = AuthenticationService(config)
-  val inq  = async.boundedQueue[Event](10)
-  val out  = async.topic[SeqexecEvent]()
-  val queues = (inq, out)
+  val inq: Queue[Event] = async.boundedQueue[Event](10)
+  val out: Topic[SeqexecEvent] = async.topic[SeqexecEvent]()
+  val queues: (Queue[Event], Topic[SeqexecEvent]) = (inq, out)
 
-  val service = new SeqexecUIApiRoutes(authService, queues, engine).service
+  val service: Service[Request, Response] = new SeqexecUIApiRoutes(authService, queues, engine).service
 
   "SeqexecUIApiRoutes login" should
     "reject requests without body" in {
@@ -88,4 +94,45 @@ class SeqexecUIApiRoutesSpec extends FlatSpec with Matchers with UriFunctions wi
         logoutResp   <- OptionT(service.apply(Request(method = Method.POST, uri = uri("/seqexec/logout")).putHeaders(authCookie)).map(Option.apply))
       } yield logoutResp
     }
+
+  val handshakeHeaders: List[Header] = List(
+    Header("Upgrade", "websocket"),
+    Header("Connection", "Upgrade"),
+    Header("Sec-WebSocket-Key", "GIBYEYWBkPl1qZfZQydmHw==htpp4"),
+    Header("Sec-WebSocket-Version", "13"),
+    Header("Sec-WebSocket-Extensions", "permessage-deflate; client_max_window_bits"),
+    Header("Origin", "http://localhost"))
+
+  "SeqexecUIApiRoutes events" should
+    "return no user if logged anonymously" in {
+      val openEvent =
+        for {
+          events     <- OptionT(service.apply(Request(uri = uri("/seqexec/events"), method = Method.GET).putHeaders(handshakeHeaders: _*)).map(Option.apply))
+          exchange   <- OptionT(Task.now(events.attributes.get(org.http4s.server.websocket.websocketKey).map(_.exchange)))
+          frames     <- OptionT(exchange.run(Process.empty).take(1).runLog.map(Option.apply))
+          firstFrame <- OptionT(Task.now(frames.headOption.collect {case WebsocketBits.Binary(data, _) => data}))
+          firstEvent <- OptionT(Task.now(Unpickle[SeqexecEvent].fromBytes(ByteBuffer.wrap(firstFrame))).map(Option.apply))
+        } yield firstEvent
+
+      openEvent.run.unsafePerformSync shouldBe Some(ConnectionOpenEvent(None))
+    }
+    it should "return the user if the cookie is provided" in {
+      val b = emit(ByteVector.view(Pickle.intoBytes(UserLoginRequest("telops", "pwd"))))
+
+      val openEvent =
+        for {
+          loginResp    <- OptionT(service.apply(Request(method = Method.POST, uri = uri("/seqexec/login"), body = b)).map(Option.apply))
+          cookieHeader =  loginResp.headers.find(_.name === "Set-Cookie".ci)
+          setCookie    <- OptionT(Task.now(cookieHeader.flatMap(u => `Set-Cookie`.parse(u.value).toOption)))
+          authCookie   =  new CookieHeader(nels(setCookie.cookie))
+          events       <- OptionT(service.apply(Request(uri = uri("/seqexec/events"), method = Method.GET).putHeaders(handshakeHeaders: _*).putHeaders(authCookie)).map(Option.apply))
+          exchange     <- OptionT(Task.now(events.attributes.get(org.http4s.server.websocket.websocketKey).map(_.exchange)))
+          frames       <- OptionT(exchange.run(Process.empty).take(1).runLog.map(Option.apply))
+          firstFrame   <- OptionT(Task.now(frames.headOption.collect {case WebsocketBits.Binary(data, _) => data}))
+          firstEvent   <- OptionT(Task.now(Unpickle[SeqexecEvent].fromBytes(ByteBuffer.wrap(firstFrame))).map(Option.apply))
+        } yield firstEvent
+
+      openEvent.run.unsafePerformSync shouldBe Some(ConnectionOpenEvent(Some(UserDetails("telops", "Telops"))))
+    }
+
 }
