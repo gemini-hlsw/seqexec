9fb9ac54a622b219735ef217f03852c557298263
Rob Norris <rob_norris@mac.com>
2017-02-24 16:16:18 -0800

    cleanup parser

--- modules/ctl/src/main/scala/opts.scala
+++ modules/ctl/src/main/scala/opts.scala
@@ -12,20 +12,43 @@ object opts {
     standalone:  Boolean
   )
 
-  val sample: Parser[Config] =
-    (
-      strOption(short('H'), long("host"),    metavar("[USER@]HOST"), value("localhost"), help("Docker user and host, localhost if unspecified. Passwordless SSH access required."))    |@|
-      strOption(short('d'), long("deploy"),  metavar("REVISION"),    value("HEAD"),      help("Revision to deploy, HEAD if unspecified."))    |@|
-      strOption(short('b'), long("base"),    metavar("REVISION"),                         help("Base revision to upgrade from. Must be an ancestor of the base revision. Defaults to the most recent version tagged 'deploy-xxx'. Cannot be specified with --standalone."), value(null)).map(Option(_)) |@|
-      switch(   short('s'), long("standalone"),                                           help("Deploy standalone; do not attempt an upgrade. Cannot be specified with --base"))
-    )(Config.apply)
+  val host: Parser[String] =
+    strOption(
+      short('H'), long("host"), metavar("[USER@]HOST"), value("localhost"),
+      help("Docker user and host, localhost if unspecified. Passwordless SSH access required.")
+    )
 
-  val opts = info(
-    subparser(command("deploy", info(sample <* helper, progDesc("Deploy an application. This is a very long description indeed. Will it wrap? We must test it out I guess.")))) <* helper,
-    progDesc("Deploy and control gem."))
+  val deploy: Parser[String] =
+    strOption(
+      short('d'), long("deploy"), metavar("REVISION"), value("HEAD"),
+      help("Revision to deploy, HEAD if unspecified.")
+    )
+
+  val base: Parser[Option[String]] =
+    strOption(
+      short('b'), long("base"), metavar("REVISION"), value(null),
+      help("Base revision to upgrade from. Must be an ancestor of the base revision. Defaults to the most recent version tagged 'deploy-xxx'. Cannot be specified with --standalone.")
+    ).map(Option(_))
+
+  val standalone: Parser[Boolean] =
+    switch(
+      short('s'), long("standalone"),
+      help("Deploy standalone; do not attempt an upgrade. Cannot be specified with --base")
+    )
+
+  val config: Parser[Config] =
+    (host |@| deploy |@| base |@| standalone)(Config.apply)
+
+  val configCommand: Parser[Config] =
+    subparser(command("deploy", info(config <* helper,
+      progDesc("Deploy an application. This is a very long description indeed. Will it wrap? We must test it out I guess."))
+    ))
+
+  val mainParser: ParserInfo[Config] =
+    info(configCommand <* helper, progDesc("Deploy and control gem."))
 
   def parse[A](progName: String, args: List[String]): IO[Option[Config]] =
-    execParserPure(prefs(idm[PrefsMod]), opts, args) match {
+    execParserPure(prefs(idm[PrefsMod]), mainParser, args) match {
       case Success(c) => IO(Some(c))
       case Failure(f) =>
         import Predef._
