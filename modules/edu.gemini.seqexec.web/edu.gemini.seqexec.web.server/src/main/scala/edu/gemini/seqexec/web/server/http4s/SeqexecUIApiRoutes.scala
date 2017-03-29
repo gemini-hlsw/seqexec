f1b7163d2836c66105b3d6f9440205f1f0fa1bdd
Danny Navarro <j@dannynavarro.net>
2017-03-29 17:35:19 -0300

    Respond with a dummy SequencesQueue when loading a Sequence
    This avoids a brief error message that was showing in the UI while loading. The
    default conditions returned don't seem to used anywhere.

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/SeqexecUIApiRoutes.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/http4s/SeqexecUIApiRoutes.scala
@@ -106,7 +106,8 @@ class SeqexecUIApiRoutes(auth: AuthenticationService, events: (engine.EventQueue
                     \/.fromTryCatchNonFatal(new SPObservationID(oid))
                       .fold(Task.fail, Task.now)
                 u     <- se.load(inputQueue, obsId)
-                resp  <- u.fold(_ => NotFound(s"Not found sequence $oid"), _ => Ok(oid))
+                resp  <- u.fold(_ => NotFound(s"Not found sequence $oid"), _ =>
+                  Ok(SequencesQueue[SequenceId](Conditions.default, List(oid))))
               } yield resp
             }.handleWith {
               case e: SPBadIDException => BadRequest(s"Bad sequence id $oid")
