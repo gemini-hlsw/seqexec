011d6a75a7de01f6ae304b16d1a92304ec00df31
Javier Lührs <jluhrs@gemini.edu>
2017-05-10 10:33:15 -0300

    SEQNG-208 Corrected default values for keywords.

--- modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/Header.scala
+++ modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/Header.scala
@@ -36,9 +36,9 @@ object Header {
   object Defaults {
     implicit def fromStringOption(v: SeqAction[Option[String]]): SeqAction[String] = v.map(_.getOrElse("No Value"))
 
-    implicit def fromDoubleOption(v: SeqAction[Option[Double]]): SeqAction[Double] = v.map(_.getOrElse(9999.0))
+    implicit def fromDoubleOption(v: SeqAction[Option[Double]]): SeqAction[Double] = v.map(_.getOrElse(-9999.0))
 
-    implicit def fromIntOption(v: SeqAction[Option[Int]]): SeqAction[Int] = v.map(_.getOrElse(9999))
+    implicit def fromIntOption(v: SeqAction[Option[Int]]): SeqAction[Int] = v.map(_.getOrElse(-9999))
 
     implicit def fromBooleanOption(v: SeqAction[Option[Boolean]]): SeqAction[Boolean] = v.map(_.getOrElse(false))
   }
