3f375e29660a643a55ff0f67fd4840026750e8c1
Carlos Quiroz <cquiroz@gemini.edu>
2017-05-15 22:27:06 -0400

    SEQNG-227 Parametrize the call for observation title #time 20m

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/odbclient/ODBClient.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.server/src/main/scala/edu/gemini/seqexec/web/server/odbclient/ODBClient.scala
@@ -3,6 +3,8 @@ package edu.gemini.seqexec.web.server.odbclient
 import scalaz.Kleisli
 import knobs.Config
 import scalaz.concurrent.Task
+import edu.gemini.spModel.core.SPProgramID
+import edu.gemini.seqexec.model.Model.SequenceId
 import org.http4s.client.blaze._
 import org.http4s.Uri
 
@@ -10,9 +12,9 @@ case class ODBClientConfig(odbHost: String)
 
 case class ODBClient(config: ODBClientConfig) {
   val httpClient = PooledHttp1Client()
-  def observationTitle: Task[String] = {
+  def observationTitle(id: SPProgramID, obsId: SequenceId): Task[String] = {
     val baseUri = s"${config.odbHost}/odbbrowser/observations"
-    val target = Uri.fromString(baseUri).toOption.get +?("programReference", "GS-2017A-Q-0")
+    val target = Uri.fromString(baseUri).toOption.get +?("programReference", id.stringValue)
     httpClient.expect[String](target)
   }
 }
