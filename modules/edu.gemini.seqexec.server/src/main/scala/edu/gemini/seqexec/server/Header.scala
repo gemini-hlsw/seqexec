d8554c7429da696f6dd6a32fdb77f47b01979b2e
Carlos Quiroz <cquiroz@gemini.edu>
2017-05-25 13:20:15 -0400

    Use a typeclass to simplify the implicit conversions

--- modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/Header.scala
+++ modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/Header.scala
@@ -3,20 +3,20 @@ package edu.gemini.seqexec.server
 import edu.gemini.seqexec.model.dhs.ImageFileId
 import edu.gemini.seqexec.server.DhsClient._
 
-/**
-  * Created by jluhrs on 1/31/17.
-  */
+import scalaz._
+import Scalaz._
+
 trait Header {
   def sendBefore(id: ImageFileId, inst: String): SeqAction[Unit]
   def sendAfter(id: ImageFileId, inst: String): SeqAction[Unit]
 }
 
 object Header {
-
+  // Default values for FITS headers
   val IntDefault: Int = -9999
   val DoubleDefault: Double = -9999.0
-  val StrDefault = "No Value"
-  val BooleanDefault = false
+  val StrDefault: String = "No Value"
+  val BooleanDefault: Boolean = false
 
   def buildKeyword[A](get: SeqAction[A], name: String, f: (String, A) => DhsClient.Keyword[A]): KeywordBag => SeqAction[KeywordBag] =
     k => get.map(x => k.add(f(name, x)))
@@ -40,37 +40,29 @@ object Header {
 
 
   object Implicits {
-    implicit class String2SeqAction(val v: Option[String]) extends AnyVal {
-      def toSeqAction: SeqAction[String] = SeqAction(v.getOrElse(Header.StrDefault))
+    // A simple typeclass to encapsulate default values
+    trait DefaultValue[A] {
+      def default: A
+    }
+    implicit class DefaultValueOps[A](a: Option[A])(implicit d: DefaultValue[A]) {
+      def orDefault: A = d.default
+    }
+    implicit val IntDefaultValue = new DefaultValue[Int] {
+      val default = IntDefault
+    }
+    implicit val DoubleDefaultValue = new DefaultValue[Double] {
+      val default = DoubleDefault
+    }
+    implicit val StrDefaultValue = new DefaultValue[String] {
+      val default = StrDefault
     }
 
-    implicit class Int2SeqAction(val v: Option[Int]) extends AnyVal {
-      def toSeqAction: SeqAction[Int] = SeqAction(v.getOrElse(Header.IntDefault))
+    implicit class A2SeqAction[A: DefaultValue](val v: Option[A]) {
+      def toSeqAction: SeqAction[A] = SeqAction(v.orDefault)
     }
 
-    implicit class Double2SeqAction(val v: Option[Double]) extends AnyVal {
-      def toSeqAction: SeqAction[Double] = SeqAction(v.getOrElse(Header.DoubleDefault))
-    }
-
-    implicit class Boolean2SeqAction(val v: Option[Boolean]) extends AnyVal {
-      def toSeqAction: SeqAction[Boolean] = SeqAction(v.getOrElse(Header.BooleanDefault))
-    }
-
-    implicit class SeqActionString2SeqAction(val v: SeqAction[Option[String]]) extends AnyVal {
-      def orDefault: SeqAction[String] = v.map(_.getOrElse(Header.StrDefault))
-    }
-
-    implicit class SeqActionInt2SeqAction(val v: SeqAction[Option[Int]]) extends AnyVal {
-      def orDefault: SeqAction[Int] = v.map(_.getOrElse(Header.IntDefault))
-    }
-
-    implicit class SeqActionDouble2SeqAction(val v: SeqAction[Option[Double]]) extends AnyVal {
-      def orDefault: SeqAction[Double] = v.map(_.getOrElse(Header.DoubleDefault))
-    }
-
-    implicit class SeqActionBoolean2SeqAction(val v: SeqAction[Option[Boolean]]) extends AnyVal {
-      def orDefault: SeqAction[Boolean] = v.map(_.getOrElse(Header.BooleanDefault))
+    implicit class SeqActionOption2SeqAction[A: DefaultValue](val v: SeqAction[Option[A]]) {
+      def orDefault: SeqAction[A] = v.map(_.orDefault)
     }
   }
-
 }
