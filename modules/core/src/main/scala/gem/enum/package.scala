ce672808c8499ce587980b29a06c52dd20726a2c
Rob Norris <rob_norris@mac.com>
2017-06-19 14:44:53 -0700

    add headers

--- modules/core/src/main/scala/gem/enum/package.scala
+++ modules/core/src/main/scala/gem/enum/package.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-2017 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package gem
 
 // The members of this package are generated from database tables, which are the source of truth.
