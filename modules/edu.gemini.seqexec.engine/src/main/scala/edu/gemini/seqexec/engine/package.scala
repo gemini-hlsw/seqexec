jdnavarro/ocs3

Add step status keyword extractor

modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/package.scala
@@ -106,7 +106,7 @@ package object engine {
   /**
     * Loads a sequence
     */
-  def load(id: Sequence.Id, seq: Sequence[Action]): Handle[Unit] =
+  def load(id: Sequence.Id, seq: Sequence[Action \/ Result]): Handle[Unit] =
     modify(
       st => Engine.State(
         st.conditions,

gemini-hlsw/seqexec

modules/edu.gemini.seqexec.engine/src/main/scala/edu/gemini/seqexec/engine/package.scala
@@ -142,7 +142,7 @@ package object engine {
   /**
     * Loads a sequence
     */
-  def load(id: Sequence.Id, seq: Sequence[Action]): HandleP[Unit] =
+  def load(id: Sequence.Id, seq: Sequence[Action \/ Result]): HandleP[Unit] =
     modify(
       st => Engine.State(
         st.conditions,