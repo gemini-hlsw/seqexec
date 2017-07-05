1699fd82b32fdcd407122ee71a6f1818f82af8ed
Rob Norris <rob_norris@mac.com>
2017-07-05 12:20:33 -0700

    threadsafe ioref

--- modules/ctl/src/main/scala/ioref.scala
+++ modules/ctl/src/main/scala/ioref.scala
@@ -4,14 +4,16 @@
 package gem.ctl
 
 import cats.effect.IO
+import java.util.concurrent.atomic.AtomicReference
 
-// N.B. this isn't in cats-effect yet
-@SuppressWarnings(Array("org.wartremover.warts.Var"))
-final class IORef[A] private (var value: A) {
-  def mod(f: A => A): IO[Unit] = IO(value = f(value))
-  def read: IO[A] = IO(value)
+/** A threadsafe mutable cell. Note that there is no notion of atomicity; all oprations race. */
+final class IORef[A] private (ref: AtomicReference[A]) {
+  // N.B. can't compare and swap here because
+  def mod(f: A => A): IO[Unit] = IO(ref.set(f(ref.get)))
+  def set(a: A): IO[Unit] = IO(ref.set(a))
+  def read: IO[A] = IO(ref.get)
 }
 object IORef {
   def apply[A](a: A): IO[IORef[A]] =
-    IO(new IORef(a))
+    IO(new IORef(new AtomicReference(a)))
 }
