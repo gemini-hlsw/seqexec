df6c395f5a933d5418009e506f173f983310f764
Rob Norris <rob_norris@mac.com>
2017-07-13 13:30:35 -0700

    instances

--- modules/core/src/main/scala/gem/Observation.scala
+++ modules/core/src/main/scala/gem/Observation.scala
@@ -5,6 +5,7 @@ package gem
 
 import scalaz._, Scalaz._
 
+/** An observation, parameterized over the types of its static config and steps. */
 final case class Observation[S, D](
   id: Observation.Id,
   title: String,
@@ -13,6 +14,7 @@ final case class Observation[S, D](
 
 object Observation {
 
+  /** An observation is identified by its program and a serial index. */
   final case class Id(pid: Program.Id, index: Int) {
     def format: String =
       s"${pid.format}-$index"
@@ -32,6 +34,7 @@ object Observation {
     def unsafeFromString(s: String): Observation.Id =
       fromString(s).getOrElse(sys.error("Malformed Observation.Id: " + s))
 
+    /** Observations are ordered by program id and index. */
     implicit val OrderId: Order[Id] =
       Order[Program.Id].contramap[Id](_.pid)   |+|
       Order[Int]       .contramap[Id](_.index)
@@ -44,6 +47,7 @@ object Observation {
 
   }
 
+  /** Observation is a bitraversable functor. */
   implicit val ObservationBitraverse: Bitraverse[Observation] =
     new Bitraverse[Observation] {
       def bitraverseImpl[G[_]: Applicative, A, B, C, D](fab: Observation[A,B])(f: A => G[C], g: B => G[D]): G[Observation[C,D]] =
