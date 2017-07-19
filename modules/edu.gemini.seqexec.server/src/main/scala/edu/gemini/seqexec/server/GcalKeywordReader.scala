dcc540231fb64a945bfb6321d847e17e651b2319
Javier Lührs <jluhrs@gemini.edu>
2017-07-19 09:55:09 -0400

    SEQNG-295 Change computation of GCAL lamp.

--- modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/GcalKeywordReader.scala
+++ modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/GcalKeywordReader.scala
@@ -32,17 +32,17 @@ object GcalKeywordsReaderImpl extends GcalKeywordReader {
   def getFilter: SeqAction[Option[String]] = GcalEpics.instance.filter
 
   def getLamp: SeqAction[Option[String]] = {
-    def ar   = GcalEpics.instance.lampAr().filter(_ == BinaryOnOff.ON) *> "Ar".some
-    def cuAr = GcalEpics.instance.lampCuAr().filter(_ == BinaryOnOff.ON) *> "CuAr".some
-    def ir   = GcalEpics.instance.lampIr().map{
+    val ar   = GcalEpics.instance.lampAr().filter(_ == BinaryOnOff.ON) *> "Ar".some
+    val cuAr = GcalEpics.instance.lampCuAr().filter(_ == BinaryOnOff.ON) *> "CuAr".some
+    val ir   = GcalEpics.instance.lampIr().map{
       case BinaryOnOff.ON => "IRhigh"
       case _              => "IRlow"
     }
-    def qh   = GcalEpics.instance.lampQH().filter(_ == BinaryOnOff.ON) *> "QH".some
-    def thAr = GcalEpics.instance.lampThAr().filter(_ == BinaryOnOff.ON) *> "ThAr".some
-    def xe   = GcalEpics.instance.lampXe().filter(_ == BinaryOnOff.ON) *> "Xe".some
+    val qh   = GcalEpics.instance.lampQH().filter(_ == BinaryOnOff.ON) *> "QH".some
+    val thAr = GcalEpics.instance.lampThAr().filter(_ == BinaryOnOff.ON) *> "ThAr".some
+    val xe   = GcalEpics.instance.lampXe().filter(_ == BinaryOnOff.ON) *> "Xe".some
 
-    Stream(ar, xe, cuAr, thAr, qh, ir).flatten.headOption
+    ar.orElse(xe).orElse(cuAr).orElse(thAr).orElse(qh).orElse(ir)
   }
 
   def getShutter: SeqAction[Option[String]] = GcalEpics.instance.shutter.map{
