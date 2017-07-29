c43d8803445ca5d1df05a70bb064bf3a9890c2a2
Carlos Quiroz <cquiroz@gemini.edu>
2017-07-29 18:02:36 -0400

    Use the typelevel compiler for both jvm and js

--- project/Common.scala
+++ project/Common.scala
@@ -16,29 +16,13 @@ object Common {
     ))),
 
     // Common libraries
-    libraryDependencies ++= Seq(ScalaZCore.value) ++ TestLibs.value,
-    // This is needed to support the TLS compiler and scala.js at the same time
-    // Maybe eventually be used when we can compile both jvm and js with typelevel
-    libraryDependencies ~= { (libDeps: Seq[ModuleID]) =>
-      libDeps.filterNot(dep => dep.name == "scalajs-compiler")
-    }
+    libraryDependencies ++= Seq(ScalaZCore.value) ++ TestLibs.value
   )
 
-  lazy val commonJSSettings = Seq(
-    // The typelevel compiler is not compatible with JS
-    // scalaOrganization := "org.typelevel",
-    scalaVersion := Settings.LibraryVersions.scalaJSVersion,
-    scalacOptions ++= Settings.Definitions.scalacOptions,
-    scalacOptions ~= (_.filterNot(Set(
-      // typelevel option not supported on js
-      "-Yinduction-heuristics",
-      "-Yliteral-types",
-      "-Xstrict-patmat-analysis",
-      "-Xlint:strict-unsealed-patmat"
-    ))),
-
-    // Common libraries
-    libraryDependencies ++= Seq(ScalaZCore.value) ++ TestLibs.value
+  lazy val commonJSSettings = commonSettings ++ Seq(
+    // These settings allow to use TLS with scala.js
+    // Remove the dependency on the scalajs-compiler
+    libraryDependencies := libraryDependencies.value.filterNot(_.name == "scalajs-compiler")
   )
 
   // This function allows triggered compilation to run only when scala files changes
