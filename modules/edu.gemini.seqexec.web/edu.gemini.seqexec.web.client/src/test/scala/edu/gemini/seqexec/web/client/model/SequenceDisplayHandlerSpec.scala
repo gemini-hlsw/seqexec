a8f3c1ea03c620dabef0f534a291319f605fe488
Carlos Quiroz <cquiroz@gemini.edu>
2017-08-07 16:42:34 -0400

    SEQNG-305 Fix for broken tests

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/test/scala/edu/gemini/seqexec/web/client/model/SequenceDisplayHandlerSpec.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/test/scala/edu/gemini/seqexec/web/client/model/SequenceDisplayHandlerSpec.scala
@@ -2,7 +2,7 @@ package edu.gemini.seqexec.web.client.model
 
 import diode.ActionResult.ModelUpdate
 import diode.RootModelRW
-import edu.gemini.seqexec.model.Model.SequenceView
+import edu.gemini.seqexec.model.Model.{SeqexecSite, SequenceView}
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -14,10 +14,10 @@ class SequenceDisplayHandlerSpec extends FlatSpec with Matchers with PropertyChe
 
   "SequenceDisplayHandler" should "ignore setting a sequence for an unknown sequence" in {
     forAll { (sequence: SequenceView) =>
-      val handler = new SequenceDisplayHandler(new RootModelRW((SequencesOnDisplay.empty, SeqexecUIModel.noSequencesLoaded)))
+      val handler = new SequenceDisplayHandler(new RootModelRW((SequencesOnDisplay.empty, SeqexecUIModel.noSequencesLoaded, SeqexecSite.SeqexecGS)))
       val result = handler.handle(SelectToDisplay(sequence))
       result should matchPattern {
-        case ModelUpdate((SequencesOnDisplay(t), _)) if t.findNext(_.sequence === SeqexecCircuit.sequenceRef(sequence.id)).isEmpty =>
+        case ModelUpdate((SequencesOnDisplay(t), _, _)) if t.findNext(_.sequence === SeqexecCircuit.sequenceRef(sequence.id)).isEmpty =>
       }
     }
   }
--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/test/scala/edu/gemini/seqexec/web/client/model/WebSocketsEventsHandlerSpec.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/test/scala/edu/gemini/seqexec/web/client/model/WebSocketsEventsHandlerSpec.scala
@@ -12,13 +12,13 @@ import Scalaz._
 class WebSocketsEventsHandlerSpec extends FlatSpec with Matchers {
 
   "WebSocketsEventsHandler" should "accept connection open events anonymously" in {
-    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2))))
+    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user, m.site))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2), site = v._3)))
     val result = handler.handle(ServerMessage(ConnectionOpenEvent(None)))
     // No user set
     result.newModelOpt.flatMap(_.uiModel.user) shouldBe None
   }
   it should "set the user if the response contains one" in {
-    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2))))
+    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user, m.site))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2), site = v._3)))
     val user = UserDetails("user", "User name")
     val result = handler.handle(ServerMessage(ConnectionOpenEvent(Some(user))))
     // No user set
@@ -26,7 +26,7 @@ class WebSocketsEventsHandlerSpec extends FlatSpec with Matchers {
   }
   it should "accept a loaded SequencesQueue" in {
     val sequences = SequencesQueue(Conditions.default, None, List.empty[SequenceView])
-    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2))))
+    val handler = new WebSocketEventsHandler(zoomRW(m => (m.uiModel.sequences, m.uiModel.user, m.site))((m, v) => m.copy(uiModel = m.uiModel.copy(sequences = v._1, user = v._2), site = v._3)))
     val result = handler.handle(ServerMessage(SequenceLoaded("TESTS-1", sequences)))
     // No user set
     result.newModelOpt.exists(_.uiModel.sequences === sequences) shouldBe true
