08605992467b64b512fe62445c9a04105c3466e0
Carlos Quiroz <cquiroz@gemini.edu>
2017-09-09 23:30:39 -0300

    SEQNG-364 Added method to emit beeps

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/services/Audio.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/services/Audio.scala
@@ -5,6 +5,8 @@ package edu.gemini.seqexec.web.client.services
 
 import scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
+import org.scalajs.dom.raw.AudioContext
+import scala.scalajs.js.timers._
 
 /**
   * JS Facade for HTML5 audio
@@ -14,4 +16,28 @@ import scala.scalajs.js.annotation.JSGlobal
 class Audio(val src: String) extends js.Object {
   def play(): js.Any = js.native
   def pause(): js.Any = js.native
+}
+
+object Beep {
+  private val ctx = new AudioContext
+
+  /** Emits a beep with the Web Audio API */
+  def beep(): SetTimeoutHandle = {
+    println("STEO")
+    val oscillator = ctx.createOscillator
+    val gain = ctx.createGain
+
+    oscillator.connect(gain)
+    gain.connect(ctx.destination)
+
+    // Volume
+    gain.gain.value = 1
+    // frequency
+    oscillator.frequency.value = 3020
+    // type
+    oscillator.`type` = "square"
+
+    oscillator.start()
+    setTimeout(150) { oscillator.stop() }
+  }
 }
