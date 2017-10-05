5548fe7c8d4a780ccec5a791153f2ce71e606db3
Carlos Quiroz <cquiroz@gemini.edu>
2017-10-05 15:34:11 -0300

    Library updates

--- modules/edu.gemini.p1backend/edu.gemini.p1backend.client/src/main/scala/edu/gemini/p1backend/client/P1BackendApp.scala
+++ modules/edu.gemini.p1backend/edu.gemini.p1backend.client/src/main/scala/edu/gemini/p1backend/client/P1BackendApp.scala
@@ -3,12 +3,10 @@
 
 package edu.gemini.p1backend.client
 
-import scala.scalajs.js.JSApp
-
 /**
   * Phase1 Backend WebApp entry point
   */
-object P1BackendApp extends JSApp {
+object P1BackendApp {
 
   def main(): Unit = {
     // Launch the application
