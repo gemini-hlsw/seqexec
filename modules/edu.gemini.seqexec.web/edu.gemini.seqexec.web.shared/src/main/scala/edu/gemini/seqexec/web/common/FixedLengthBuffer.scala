37cdbdb2d30e71472f25f7d9bf8ec4fae35ab79c
Carlos Quiroz <cquiroz@gemini.edu>
2017-11-21 17:11:25 -0300

    SEQNG-347 Define foldable instance for FixedLengthBuffer #time 1h

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.shared/src/main/scala/edu/gemini/seqexec/web/common/FixedLengthBuffer.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.shared/src/main/scala/edu/gemini/seqexec/web/common/FixedLengthBuffer.scala
@@ -3,7 +3,7 @@
 
 package edu.gemini.web.common
 
-import scalaz.{Show, Equal, Functor}
+import scalaz.{Show, Equal, Foldable, IsEmpty, Functor}
 import scalaz.syntax.std.boolean._
 import scalaz.syntax.equal._
 import scalaz.std.AllInstances._
@@ -27,6 +27,10 @@ object FixedLengthBuffer {
 
     def toVector: Vector[A] =
       data
+
+    def size: Int = data.size
+
+    def isEmpty: Boolean = data.isEmpty
   }
 
   def apply[A](maxLength: Int, initial: A*): Option[FixedLengthBuffer[A]] = {
@@ -40,14 +44,41 @@ object FixedLengthBuffer {
   def unsafeFromInt[A](maxLength: Int, initial: A*): FixedLengthBuffer[A] =
     fromInt[A](maxLength, initial: _*).getOrElse(sys.error(s"Invalid max length $maxLength, data length ${initial.length}"))
 
+  def Zero[A]: FixedLengthBuffer[A] = FixedLengthBufferImpl[A](0, Vector.empty[A])
+
   implicit def show[A]: Show[FixedLengthBuffer[A]] = Show.showFromToString
 
   implicit def equal[A]: Equal[FixedLengthBuffer[A]] = Equal.equalA
 
-  implicit def functor[A]: Functor[FixedLengthBuffer] = new Functor[FixedLengthBuffer] {
+  implicit val functor: Functor[FixedLengthBuffer] = new Functor[FixedLengthBuffer] {
     def map[B, C](fa: FixedLengthBuffer[B])(f: B => C): FixedLengthBuffer[C] = fa match {
         case FixedLengthBufferImpl(max, data) => FixedLengthBufferImpl[C](max, data.map(f))
       }
+  }
+
+  implicit val foldable: Foldable[FixedLengthBuffer] with IsEmpty[FixedLengthBuffer] = new Foldable[FixedLengthBuffer] with IsEmpty[FixedLengthBuffer] with Foldable.FromFoldr[FixedLengthBuffer] {
+    override def length[A](fa: FixedLengthBuffer[A]) = fa.size
+    def empty[A] = Zero[A]
+    def plus[A](a: FixedLengthBuffer[A], b: => FixedLengthBuffer[A]) = FixedLengthBufferImpl(a.maxLength + b.maxLength, a.toVector ++ b.toVector)
+    def isEmpty[A](fa: FixedLengthBuffer[A]) = fa.isEmpty
+
+    @SuppressWarnings(Array("org.wartremover.warts.Var", "org.wartremover.warts.While"))
+    def foldRight[A, B](fa: FixedLengthBuffer[A], z: => B)(f: (A, => B) => B) = {
+      import scala.collection.mutable.ArrayStack
+      val s = new ArrayStack[A]
+      fa.toVector.foreach(a => s += a)
+      var r = z
+      while (!s.isEmpty) {
+        // Fixes stack overflow issue (#866)
+        val w = r
+        r = f(s.pop, w)
+      }
+      r
+    }
+    override def all[A](fa: FixedLengthBuffer[A])(f: A => Boolean) =
+      fa.toVector.forall(f)
+    override def any[A](fa: FixedLengthBuffer[A])(f: A => Boolean) =
+      fa.toVector.exists(f)
   }
 
 }
@@ -71,4 +102,11 @@ sealed trait FixedLengthBuffer[A] {
    * Converts the buffer to a list
    */
   def toVector: Vector[A]
+
+  /**
+   * Size of the data
+   */
+  def size: Int
+
+  def isEmpty: Boolean
 }
