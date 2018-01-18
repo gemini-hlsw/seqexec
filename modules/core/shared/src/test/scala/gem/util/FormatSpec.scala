8bb8e1b156883aa6e2c06ae4c13dbe6a67fb55d0
Rob Norris <rob_norris@mac.com>
2018-01-18 11:23:30 -0800

    unsafeGet tests

---
+++ modules/core/shared/src/test/scala/gem/util/FormatSpec.scala
@@ -0,0 +1,44 @@
+// Copyright (c) 2016-2017 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package gem
+package util
+
+import cats.tests.CatsSuite
+import gem.laws.discipline._
+
+final class FormatSpec extends CatsSuite {
+
+  // Our example Format injects ints into "positive and even"
+  val example: Format[Int, Boolean] =
+    Format(n => if (n > 0) Some(n % 2 == 0) else None, b => if (b) 2 else 1)
+
+  // Ensure it's lawful
+  checkAll("Formats.HmsDms", FormatTests(example).format)
+
+  test("unsafeGet.consistent with getOption") {
+    forAll { (n: Int) =>
+      example.getOption(n) shouldEqual Either.catchNonFatal(example.unsafeGet(n)).toOption
+    }
+  }
+
+  test("unsafeGet.error.tagged") {
+    forAll { (n: Int) =>
+      Either.catchNonFatal(example.unsafeGet(n)) match {
+        case Left(t)  => t.getMessage shouldEqual s"Boolean: unsafeGet failed: $n"
+        case Right(b) => true
+      }
+    }
+  }
+
+  test("unsafeGet.error.untagged") {
+    forAll { (n: Int) =>
+      def unsafeGet[A, B](f: Format[A, B], a: A): B = f.unsafeGet(a)
+      Either.catchNonFatal(unsafeGet(example, n)) match {
+        case Left(t)  => t.getMessage shouldEqual s"unsafeGet failed: $n"
+        case Right(b) => true
+      }
+    }
+  }
+
+}
