8a0d8fc2ab1be410a6c9d67aaf79eba27c485f1c
Javier Lührs <jluhrs@gemini.edu>
2018-01-31 13:14:11 -1000

    SEQNG-495 Implemented generic instrument simulator.

--- modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/gmos/GmosControllerSim.scala
+++ modules/edu.gemini.seqexec.server/src/main/scala/edu/gemini/seqexec/server/gmos/GmosControllerSim.scala
@@ -3,103 +3,40 @@
 
 package edu.gemini.seqexec.server.gmos
 
-import java.util.concurrent.atomic.AtomicBoolean
-
 import edu.gemini.seqexec.model.dhs.ImageFileId
-import edu.gemini.seqexec.server.SeqexecFailure.SeqexecException
 import edu.gemini.seqexec.server.gmos.GmosController.{GmosConfig, NorthTypes, SiteDependentTypes, SouthTypes}
-import edu.gemini.seqexec.server.{ObserveCommand, SeqAction, TrySeq}
-import gov.aps.jca.TimeoutException
-import org.log4s._
+import edu.gemini.seqexec.server.{InstrumentSim, ObserveCommand, SeqAction}
 import squants.Time
 
-import scala.annotation.tailrec
-import scalaz.{EitherT, Show}
-import scalaz.concurrent.Task
-import scalaz.syntax.show._
-import scalaz.syntax.std.boolean._
+import scalaz.Show
 
 private class GmosControllerSim[T<:SiteDependentTypes](name: String) extends GmosController[T] {
-  private val Log = getLogger
 
   implicit val configShow: Show[GmosConfig[T]] = Show.shows { config => s"(${config.cc.filter}, ${config.cc.disperser}, ${config.cc.fpu}, ${config.cc.stage}, ${config.cc.stage}, ${config.cc.dtaX}, ${config.cc.adc}, ${config.cc.useElectronicOffset}, ${config.dc.t}, ${config.dc.b}, ${config.dc.s}, ${config.dc.bi}, ${config.dc.roi.rois})" }
 
   override def getConfig: SeqAction[GmosConfig[T]] = ??? // scalastyle:ignore
 
-  private val stopFlag = new AtomicBoolean(false)
-  private val abortFlag = new AtomicBoolean(false)
-  private val pauseFlag = new AtomicBoolean(false)
+  private val sim: InstrumentSim = InstrumentSim(s"Gmos $name")
 
-  private val tic = 200
+  override def observe(obsid: ImageFileId, expTime: Time): SeqAction[ObserveCommand.Result] =
+    sim.observe(obsid, expTime)
 
-  @tailrec
-  private def observeTic(stop: Boolean, abort: Boolean, pause: Boolean, remain: Int, timeout: Option[Int]): TrySeq[ObserveCommand.Result] =
-    if(remain < tic) {
-      Log.info(s"Simulate Gmos $name observation completed")
-      TrySeq(ObserveCommand.Success)
-    } else if(stop) TrySeq(ObserveCommand.Stopped)
-      else if(abort) TrySeq(ObserveCommand.Aborted)
-      else if(pause) TrySeq(ObserveCommand.Paused)
-      else if(timeout.exists(_<= 0)) TrySeq.fail(SeqexecException(new TimeoutException()))
-      else {
-        Thread.sleep(tic.toLong)
-        observeTic(stopFlag.get, abortFlag.get, pauseFlag.get, remain-tic, timeout.map(_ - tic))
-      }
+  override def applyConfig(config: GmosConfig[T]): SeqAction[Unit] = sim.applyConfig(config)
 
-  override def observe(obsid: ImageFileId, expTime: Time): SeqAction[ObserveCommand.Result] = EitherT( Task {
-    Log.info(s"Simulate taking Gmos $name observation with label $obsid")
-    pauseFlag.set(false)
-    stopFlag.set(false)
-    abortFlag.set(false)
-    observeTic(false, false, false, 7000, (expTime.value > 0.0).option(expTime.toMilliseconds.toInt))
-  } )
+  override def stopObserve: SeqAction[Unit] = sim.stopObserve
 
-  override def applyConfig(config: GmosConfig[T]): SeqAction[Unit] = EitherT( Task {
-    Log.info(s"Simulate applying Gmos $name configuration ${config.shows}")
-    Thread.sleep(1000)
-    TrySeq(())
-  } )
+  override def abortObserve: SeqAction[Unit] = sim.abortObserve
 
-  override def stopObserve: SeqAction[Unit] = EitherT( Task {
-    Log.info(s"Simulate stopping Gmos $name exposure")
-    stopFlag.set(true)
-    TrySeq(())
-  } )
+  override def endObserve: SeqAction[Unit] = sim.endObserve
 
-  override def abortObserve: SeqAction[Unit] = EitherT( Task {
-    Log.info(s"Simulate aborting Gmos $name exposure")
-    abortFlag.set(true)
-    TrySeq(())
-  } )
+  override def pauseObserve: SeqAction[Unit] = sim.pauseObserve
 
-  override def endObserve: SeqAction[Unit] = EitherT( Task {
-    Log.info("Simulate sending endObserve to Gmos")
-    TrySeq(())
-  } )
+  override def resumePaused(expTime: Time): SeqAction[ObserveCommand.Result] = sim.resumePaused(expTime)
 
-  override def pauseObserve: SeqAction[Unit] = EitherT( Task {
-    Log.info(s"Simulate pausing Gmos $name exposure")
-    pauseFlag.set(true)
-    TrySeq(())
-  } )
+  override def stopPaused: SeqAction[ObserveCommand.Result] = sim.stopPaused
 
-  override def resumePaused(expTime: Time): SeqAction[ObserveCommand.Result] = EitherT( Task {
-    Log.info(s"Simulate resuming Gmos $name observation")
-    pauseFlag.set(false)
-    observeTic(false, false, false, 5000, (expTime.value > 0.0).option(expTime.toMilliseconds.toInt))
-  } )
+  override def abortPaused: SeqAction[ObserveCommand.Result] = sim.abortPaused
 
-  override def stopPaused: SeqAction[ObserveCommand.Result] = EitherT( Task {
-    Log.info(s"Simulate stopping Gmos $name paused observation")
-    pauseFlag.set(false)
-    observeTic(true, false, false, 1000, None)
-  } )
-
-  override def abortPaused: SeqAction[ObserveCommand.Result] = EitherT( Task {
-    Log.info(s"Simulate aborting Gmos $name paused observation")
-    pauseFlag.set(false)
-    observeTic(false, true, false, 1000, None)
-  } )
 }
 
 object GmosControllerSim {
