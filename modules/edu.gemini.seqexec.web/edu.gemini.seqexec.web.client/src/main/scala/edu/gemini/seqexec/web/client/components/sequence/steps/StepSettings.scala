e6d3bfe42e8132684681efbadc26b002dbb75574
Carlos Quiroz <cquiroz@gemini.edu>
2018-02-06 15:41:25 -0300

    SEQNG-478 Properly position the skip step icon

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/sequence/steps/StepSettings.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/components/sequence/steps/StepSettings.scala
@@ -108,9 +108,10 @@ object StepIconCell {
 
   private def stepStyle(p: StepToolsCell.Props): StyleA =
     p.step.status match {
-      case StepState.Running                                  => SeqexecStyles.runningIconCell
-      case StepState.Skipped                                  => SeqexecStyles.skippedIconCell
-      case _                                                  => SeqexecStyles.iconCell
+      case StepState.Running => SeqexecStyles.runningIconCell
+      case StepState.Skipped => SeqexecStyles.skippedIconCell
+      case _ if p.step.skip  => SeqexecStyles.skippedIconCell
+      case _                 => SeqexecStyles.iconCell
     }
 
   private val component = ScalaComponent.builder[StepToolsCell.Props]("StepIconCell")
