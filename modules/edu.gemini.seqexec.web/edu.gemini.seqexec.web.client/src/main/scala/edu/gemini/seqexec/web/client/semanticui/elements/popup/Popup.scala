70f06868ead9f10ce98069e865a96ae070ec79af
Carlos Quiroz <cquiroz@gemini.edu>
2018-02-13 17:10:08 -0300

    SEQNG-457 Use the js based popups on the other buttons

--- modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/semanticui/elements/popup/Popup.scala
+++ modules/edu.gemini.seqexec.web/edu.gemini.seqexec.web.client/src/main/scala/edu/gemini/seqexec/web/client/semanticui/elements/popup/Popup.scala
@@ -6,31 +6,34 @@ package edu.gemini.seqexec.web.client.semanticui.elements.popup
 import japgolly.scalajs.react.component.Scala.Unmounted
 import japgolly.scalajs.react.{Callback, ScalaComponent}
 import japgolly.scalajs.react.vdom.html_<^._
+import japgolly.scalajs.react.raw.ReactElement
 
 /**
-  * Produces a dropdown menu, similar to a combobox
+  * Produces a popup using javascript
+  * This is a barebones component waiting for the proper react component to be made available
   */
-
-@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements"))
+@SuppressWarnings(Array("org.wartremover.warts.NonUnitStatements", "org.wartremover.warts.AsInstanceOf"))
 object Popup {
   @SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
   final case class Props(selector: String, content: String)
 
   private val component = ScalaComponent.builder[Props]("Popup")
     .stateless
-    .renderPC((_, p, c) => <.div(c))
+    .renderPC{($, p, c) =>
+      // This is slightly unsafe though in most case we'll be passing a ReactElement here
+      VdomElement($.propsChildren.only().asInstanceOf[ReactElement])
+    }
     .componentDidMount(ctx =>
       Callback {
         // Enable menu on Semantic UI
         import org.querki.jquery.$
         import edu.gemini.web.client.facades.semanticui.SemanticUIPopup._
 
-        println($(ctx.getDOMNode).find(ctx.props.selector).length)
-        $(ctx.getDOMNode).find(ctx.props.selector).popup(
+        $(ctx.getDOMNode).popup(
           JsPopupOptions.content(ctx.props.content)
         )
       }
     ).build
 
-  def apply(p: Props, children: VdomNode*): Unmounted[Props, Unit, Unit] = component(p)(children: _*)
+  def apply(p: Props, children: VdomNode): Unmounted[Props, Unit, Unit] = component(p)(children)
 }
