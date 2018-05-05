6ec1abd7c4cf4a5622b7941beda0e37ca3f96699
Carlos Quiroz <cquiroz@gemini.edu>
2018-05-05 18:54:39 -0300

    SEQNG-252 Use a typeclass to limit the types of status items that can be read

--- modules/edu.gemini.giapi.client/src/main/scala/edu/gemini/giapi/client/package.scala
+++ modules/edu.gemini.giapi.client/src/main/scala/edu/gemini/giapi/client/package.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-2017 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package edu.gemini.giapi.client
+package edu.gemini.giapi
 
 import cats._
 import cats.implicits._
@@ -10,89 +10,112 @@ import edu.gemini.aspen.giapi.util.jms.status.StatusGetter
 import edu.gemini.jms.activemq.provider.ActiveMQJmsProvider
 import fs2.async
 
-/**
-  * Represents a connection to a GIAPi based instrument
-  * @tparam F Effect type
-  */
-trait GiapiConnection[F[_]] {
-  def connect: F[Giapi[F]]
+package object client {
+
+  /**
+    * Allowed types according to GIAPI
+    */
+  implicit val strItemGetter: ItemGetter[String]    = new ItemGetter[String] {}
+  implicit val doubleItemGetter: ItemGetter[Double] = new ItemGetter[Double] {}
+  implicit val intItemGetter: ItemGetter[Int]       = new ItemGetter[Int]    {}
+  implicit val floatItemGetter: ItemGetter[Float]   = new ItemGetter[Float]  {}
 }
 
-/**
-  * Algebra to interact with a GIAPI instrument
-  * @tparam F Effect Type
-  */
-trait Giapi[F[_]] {
+package client {
 
   /**
-    * Returns a value for the status item. If not found a `None` is returned
+    * Typeclass to present as evidence when calling `Giapi.get`
     */
-  def get[A](statusItem: String): F[Option[A]]
+  sealed trait ItemGetter[A]
 
   /**
-    * Close the connection
+    * Represents a connection to a GIAPi based instrument
+    *
+    * @tparam F Effect type
     */
-  def close: F[Unit]
-}
-
-/**
-  * Interpreters
-  */
-object Giapi {
-
-  /**
-    * Interpreter on Id
-    */
-  val giapiConnectionWithId: GiapiConnection[Id] = new GiapiConnection[Id] {
-
-    def connect: Id[Giapi[Id]] = new Giapi[Id] {
-
-      def get[A](statusItem: String): Id[Option[A]] =
-        None
-
-      override def close: Id[Unit] = ()
-    }
+  trait GiapiConnection[F[_]] {
+    def connect: F[Giapi[F]]
   }
 
   /**
-    * Interpreter on F
-    * @param url Url to connect to
-    * @tparam F Effect type
+    * Algebra to interact with a GIAPI instrument
+    *
+    * @tparam F Effect Type
     */
-  def giapiConnection[F[_]: Sync](url: String): GiapiConnection[F] =
-    new GiapiConnection[F] {
-      private def statusGetter(c: ActiveMQJmsProvider): F[StatusGetter] = Sync[F].delay {
-        val sg = new StatusGetter("client")
-        sg.startJms(c)
-        sg
+  trait Giapi[F[_]] {
+
+    /**
+      * Returns a value for the status item. If not found a `None` is returned
+      */
+    def get[A: ItemGetter](statusItem: String): F[Option[A]]
+
+    /**
+      * Close the connection
+      */
+    def close: F[Unit]
+  }
+
+  /**
+    * Interpreters
+    */
+  object Giapi {
+
+    /**
+      * Interpreter on Id
+      */
+    val giapiConnectionWithId: GiapiConnection[Id] = new GiapiConnection[Id] {
+
+      def connect: Id[Giapi[Id]] = new Giapi[Id] {
+
+        def get[A: ItemGetter](statusItem: String): Id[Option[A]] =
+          None
+
+        override def close: Id[Unit] = ()
       }
+    }
 
-      private def build(ref: async.Ref[F, ActiveMQJmsProvider]): F[Giapi[F]] =
-        for {
-          c  <- ref.get
-          sc <- statusGetter(c)
-        } yield
-        // Build a reference
-        new Giapi[F] {
-          override def get[A](statusItem: String): F[Option[A]] = Sync[F].delay {
-            val item = sc.getStatusItem[A](statusItem)
-            Option(item.getValue)
-          }
-
-          override def close: F[Unit] =
-            for {
-              _ <- Sync[F].delay(sc.stopJms())
-              _ <- Sync[F].delay(c.stopConnection())
-            } yield ()
+    /**
+      * Interpreter on F
+      *
+      * @param url Url to connect to
+      * @tparam F Effect type
+      */
+    def giapiConnection[F[_]: Sync](url: String): GiapiConnection[F] =
+      new GiapiConnection[F] {
+        private def statusGetter(c: ActiveMQJmsProvider): F[StatusGetter] = Sync[F].delay {
+          val sg = new StatusGetter("client")
+          sg.startJms(c)
+          sg
         }
 
-      def connect: F[Giapi[F]] =
-        for {
-          c   <- Sync[F].delay(new ActiveMQJmsProvider(url)) // Build the connection
-          ref <- async.refOf(c)                              // store a reference
-          _   <- Sync[F].delay(c.startConnection())          // Start the connection
-          c   <- build(ref)                                  // Build the interpreter
-        } yield c
-    }
-}
+        private def giapi(c: ActiveMQJmsProvider, sg: StatusGetter) =
+          new Giapi[F] {
+            override def get[A: ItemGetter](statusItem: String): F[Option[A]] = Sync[F].delay {
+              val item = sg.getStatusItem[A](statusItem)
+              Option(item.getValue)
+            }
 
+            override def close: F[Unit] =
+              for {
+                _ <- Sync[F].delay(sg.stopJms())
+                _ <- Sync[F].delay(c.stopConnection())
+              } yield ()
+          }
+
+        private def build(ref: async.Ref[F, ActiveMQJmsProvider]): F[Giapi[F]] =
+          for {
+            c  <- ref.get
+            sg <- statusGetter(c)
+          } yield giapi(c, sg)
+
+        def connect: F[Giapi[F]] =
+          for {
+            c   <- Sync[F].delay(new ActiveMQJmsProvider(url)) // Build the connection
+            ref <- async.refOf(c)                              // store a reference
+            _   <- Sync[F].delay(c.startConnection())          // Start the connection
+            c   <- build(ref)                                  // Build the interpreter
+          } yield c
+      }
+  }
+
+}
