953923867cafaaa164a2612d4e6b7824542d9f86
Rob Norris <rob_norris@mac.com>
2018-07-19 16:25:50 -0700

    work

--- build/upgrade/step-2.sh
+++ build/upgrade/step-2.sh
@@ -8,11 +8,16 @@ set -xe
 DEPLOY_SERVER=`buildkite-agent meta-data get deploy-server`
 DEPLOY_VERSION=`buildkite-agent meta-data get deploy-version`
 
+echo "--- :scala: Compiling gemctl"
+/usr/local/bin/sbt                   \
+  -jvm-opts build/buildkite-jvmopts  \
+  -Docs3.skipDependencyUpdates       \
+  ctl/compile
+
 echo "--- :docker: Deploying $DEPLOY_VERSION to $DEPLOY_SERVER"
 /usr/local/bin/sbt                   \
   -jvm-opts build/buildkite-jvmopts  \
   -Docs3.skipDependencyUpdates       \
-  ctl/compile                        \
   "ctl/runMain gem.ctl.main --verbose --host $DEPLOY_SERVER deploy-production $DEPLOY_VERSION"
 
 
