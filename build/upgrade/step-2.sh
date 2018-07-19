7f7d1faa44b7f806cb10d8e6bb38eb6c8c71c483
Rob Norris <rob_norris@mac.com>
2018-07-19 15:53:03 -0700

    work

--- build/upgrade/step-2.sh
+++ build/upgrade/step-2.sh
@@ -11,6 +11,7 @@ DEPLOY_VERSION=`buildkite-agent meta-data get deploy-version`
 echo "--- :docker: Deploying $DEPLOY_VERSION to $DEPLOY_SERVER"
 /usr/local/bin/sbt                   \
   -jvm-opts build/buildkite-jvmopts  \
+  -Docs3.skipDependencyUpdates       \
   ctl/compile
   "ctl/runMain gem.ctl.main --verbose --host $DEPLOY_SERVER deploy-production $DEPLOY_VERSION"
 
