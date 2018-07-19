b2da7a36a8ae83956131508fbdd5b07b48324622
Rob Norris <rob_norris@mac.com>
2018-07-19 15:55:30 -0700

    work

--- build/upgrade/step-2.sh
+++ build/upgrade/step-2.sh
@@ -3,7 +3,7 @@
 # This dynamically generates a pipeline containing a click to unblock step with
 # custom fields.
 
-set -e
+set -xe
 
 DEPLOY_SERVER=`buildkite-agent meta-data get deploy-server`
 DEPLOY_VERSION=`buildkite-agent meta-data get deploy-version`
@@ -12,7 +12,7 @@ echo "--- :docker: Deploying $DEPLOY_VERSION to $DEPLOY_SERVER"
 /usr/local/bin/sbt                   \
   -jvm-opts build/buildkite-jvmopts  \
   -Docs3.skipDependencyUpdates       \
-  ctl/compile
+  ctl/compile                        \
   "ctl/runMain gem.ctl.main --verbose --host $DEPLOY_SERVER deploy-production $DEPLOY_VERSION"
 
 
