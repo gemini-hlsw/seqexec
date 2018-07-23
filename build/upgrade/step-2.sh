c4d5d9a622e98aedd21d37e5c5eaafb5747ed4fb
Rob Norris <rob_norris@mac.com>
2018-07-23 11:37:29 -0700

    comments

--- build/upgrade/step-2.sh
+++ build/upgrade/step-2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-
-# This dynamically generates a pipeline containing a click to unblock step with
-# custom fields.
-
 set -xe
 
+# These are set by step-1 which blocks the pipeline and prompts for these values.
 DEPLOY_SERVER=`buildkite-agent meta-data get deploy-server`
 DEPLOY_VERSION=`buildkite-agent meta-data get deploy-version`
 
+# There is no way to pass an empty string as a property, so we pass a flag instead.
 if [ `buildkite-agent meta-data get deploy-verbose` = "true" ]; then
   DEPLOY_VERBOSE="--verbose"
 fi
 
+# First build gemctl
 echo "--- :scala: Compiling gemctl"
 /usr/local/bin/sbt                   \
   -jvm-opts build/buildkite-jvmopts  \
   -Docs3.skipDependencyUpdates       \
   ctl/compile
 
+# Now do the deploy on another invocation of sbt. Slightly slower but the logs are much cleaner.
 echo "--- :docker: Deploying $DEPLOY_VERSION to $DEPLOY_SERVER"
 /usr/local/bin/sbt                   \
   -jvm-opts build/buildkite-jvmopts  \
