76b3037ca25e2f309608ed8ec163a0fca4376021
Rob Norris <rob_norris@mac.com>
2018-08-09 09:25:03 -1000

    move enum

---
+++ modules/seqexec/model/src/main/scala/seqexec/model/enum/ActionType.scala
@@ -0,0 +1,25 @@
+// Copyright (c) 2016-2018 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package seqexec.model.enum
+
+import cats.Eq
+import cats.implicits._
+
+sealed trait ActionType extends Product with Serializable
+
+object ActionType {
+
+        case object Observe                  extends ActionType
+        case object Undefined                extends ActionType // Used in tests
+  final case class  Configure(sys: Resource) extends ActionType
+
+  implicit val equal: Eq[ActionType] =
+    Eq.instance {
+      case (Observe, Observe)           => true
+      case (Undefined, Undefined)       => true
+      case (Configure(a), Configure(b)) => a === b
+      case _                            => false
+    }
+
+}
