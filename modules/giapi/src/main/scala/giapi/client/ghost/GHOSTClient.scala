bbecffa86994081de044023be2597bfb4974188f
sraaphorst <sraaphorst@gemini.edu>
2018-10-02 10:58:55 -0300

    SEQNG-677: Trying to fix header complaint.

--- modules/giapi/src/main/scala/giapi/client/ghost/GHOSTClient.scala
+++ modules/giapi/src/main/scala/giapi/client/ghost/GHOSTClient.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-2018 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package giapi.client.ghost
 
 import cats.implicits._
