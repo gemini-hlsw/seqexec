d3109758531333d509ce5483c47beeff9a057c67
Carlos Quiroz <cquiroz@gemini.edu>
2018-10-11 13:55:36 -0300

    SEQNG-749 Remove unused field
    Signed-off-by: Carlos Quiroz <cquiroz@gemini.edu>

--- modules/seqexec/server/src/main/scala/seqexec/server/ExecutionQueue.scala
+++ modules/seqexec/server/src/main/scala/seqexec/server/ExecutionQueue.scala
@@ -8,19 +8,17 @@ import cats.implicits._
 import gem.Observation
 import monocle.macros.Lenses
 import seqexec.model.enum.BatchCommandState
-import seqexec.model.enum.BatchExecState
 
 @Lenses
 final case class ExecutionQueue(name:      String,
                                 cmdState:  BatchCommandState,
-                                execState: BatchExecState,
                                 queue:     List[Observation.Id])
 
 @SuppressWarnings(Array("org.wartremover.warts.PublicInference"))
 object ExecutionQueue {
   def init(name: String): ExecutionQueue =
-    ExecutionQueue(name, BatchCommandState.Idle, BatchExecState.Idle, List())
+    ExecutionQueue(name, BatchCommandState.Idle, List())
 
   implicit val eq: Eq[ExecutionQueue] =
-    Eq.by(x => (x.name, x.cmdState, x.execState, x.queue))
+    Eq.by(x => (x.name, x.cmdState, x.queue))
 }
