d93559234865e8b2d181e3b0e13d4087c30e0092
Javier Lührs <jluhrs@gemini.edu>
2018-12-18 12:10:11 -0300

    SEQNG-836 NIRI Epics code.

---
+++ modules/seqexec/server/src/main/java/edu/gemini/seqexec/server/niri/BeamSplitter.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016-2018 Association of Universities for Research in Astronomy, Inc. (AURA)
+ * For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package edu.gemini.seqexec.server.niri;
+
+public enum BeamSplitter {
+    F6("f/6"),
+    F14("f/14"),
+    F32("f/32"),
+    SameAsCamera("same-as-camera");
+
+    final String name;
+
+    BeamSplitter(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
---
+++ modules/seqexec/server/src/main/java/edu/gemini/seqexec/server/niri/DetectorState.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016-2018 Association of Universities for Research in Astronomy, Inc. (AURA)
+ * For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package edu.gemini.seqexec.server.niri;
+
+public enum DetectorState {
+    Inactive("Deactivate", false),
+    Active("Activate", true); // Why are the values verbs?
+
+    final String name;
+    final boolean active;
+
+    DetectorState(String name, boolean active) {
+        this.name = name;
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public boolean getActive() {
+        return this.active;
+    }
+}
