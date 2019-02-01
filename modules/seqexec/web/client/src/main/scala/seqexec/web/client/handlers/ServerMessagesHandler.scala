2c12c7bb63bc33365362bb22848f5be49ca91894
sraaphorst <sraaphorst@gemini.edu>
2019-02-01 12:55:55 -0300

    SEQNG-846: Making buildkite happy.

--- modules/seqexec/web/client/src/main/scala/seqexec/web/client/handlers/ServerMessagesHandler.scala
+++ modules/seqexec/web/client/src/main/scala/seqexec/web/client/handlers/ServerMessagesHandler.scala
@@ -34,7 +34,7 @@ import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 class ServerMessagesHandler[M](modelRW: ModelRW[M, WebSocketsFocus])
     extends ActionHandler(modelRW)
     with Handlers[M, WebSocketsFocus] {
-  
+
   // Global references to audio files
   private val SequencePausedAudio = new Audio(SequencePausedResource.resource)
   private val ExposurePausedAudio = new Audio(ExposurePausedResource.resource)
