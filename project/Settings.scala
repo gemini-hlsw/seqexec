a2aada8079a86c86183fbc6810089a2b7a38bfdc
Shane Walker <swalker@gemini.edu>
2019-06-10 12:38:05 -0400

    Tag gsp-math

--- project/Settings.scala
+++ project/Settings.scala
@@ -141,7 +141,7 @@ object Settings {
     val geminiLocales           = "0.1.0-2019a"
 
     // Gemini Libraries
-    val gspMath                 = "0.1.0+5-7322fff9-SNAPSHOT"
+    val gspMath                 = "0.1.1"
   }
 
   /**
