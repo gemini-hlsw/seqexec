09601a4533c217b2c2d51da0051b996ea8f6e3c9
Scala Steward <me@scala-steward.org>
2019-09-09 15:26:06 +0200

    Update flyway-core to 6.0.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -108,7 +108,7 @@ object Settings {
     val monocleVersion          = "1.5.1-cats"
     val circeVersion            = "0.11.1"
     val doobieVersion           = "0.6.0"
-    val flywayVersion           = "6.0.1"
+    val flywayVersion           = "6.0.2"
     val tucoVersion             = "0.4.1"
     val declineVersion          = "0.6.2"
 
