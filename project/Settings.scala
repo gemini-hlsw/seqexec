8eb52e9cc91e5a64d9f6a254a827364682fa98f4
Scala Steward <me@scala-steward.org>
2019-09-17 23:41:01 +0200

    Update doobie-core, doobie-postgres, ... to 0.8.2

--- project/Settings.scala
+++ project/Settings.scala
@@ -107,7 +107,7 @@ object Settings {
     val knobs                   = "7.0.24"
     val monocleVersion          = "1.5.1-cats"
     val circeVersion            = "0.11.1"
-    val doobieVersion           = "0.6.0"
+    val doobieVersion           = "0.8.2"
     val flywayVersion           = "6.0.1"
     val tucoVersion             = "0.4.1"
     val declineVersion          = "0.6.2"
