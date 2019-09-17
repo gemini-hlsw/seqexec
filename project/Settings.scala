489002006061ac836aece3533e2c350d2b602d9d
Scala Steward <me@scala-steward.org>
2019-09-17 23:39:37 +0200

    Update gsp-math, gsp-math-testkit to 0.1.6

--- project/Settings.scala
+++ project/Settings.scala
@@ -143,7 +143,7 @@ object Settings {
     val geminiLocales           = "0.1.0-2019a"
 
     // Gemini Libraries
-    val gspMath                 = "0.1.1"
+    val gspMath                 = "0.1.6"
   }
 
   /**
