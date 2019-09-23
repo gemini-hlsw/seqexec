952425694f47d9dda30512b1546abe11cd64fb25
Scala Steward <me@scala-steward.org>
2019-09-23 02:06:10 +0200

    Update jwt-core to 4.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -48,7 +48,7 @@ object Settings {
     val argonaut                = "6.2.3"
     val commonsHttp             = "2.0.2"
     val unboundId               = "3.2.1"
-    val jwt                     = "2.1.0"
+    val jwt                     = "4.1.0"
     val slf4j                   = "1.7.26"
     val log4s                   = "1.8.2"
     val log4cats                = "0.2.0"
