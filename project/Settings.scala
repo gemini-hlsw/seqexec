dd4ea09eaa2c394c0d73102be3a4add318e33235
Scala Steward <me@scala-steward.org>
2019-11-03 23:10:57 +0100

    Update jwt-circe, jwt-core to 4.2.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -47,7 +47,7 @@ object Settings {
     val squants                 = "1.5.0"
     val commonsHttp             = "2.0.2"
     val unboundId               = "3.2.1"
-    val jwt                     = "4.1.0"
+    val jwt                     = "4.2.0"
     val slf4j                   = "1.7.28"
     val log4s                   = "1.8.2"
     val log4cats                = "1.0.1"
