894482985cadd58b910ec390940f6c2884e3ff2a
Scala Steward <me@scala-steward.org>
2019-12-11 06:40:16 +0100

    Update squants to 1.6.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -43,7 +43,7 @@ object Settings {
     val scalaXmlVerson          = "1.2.0"
 
     val http4sVersion           = "0.21.0-M6"
-    val squants                 = "1.5.0"
+    val squants                 = "1.6.0"
     val commonsHttp             = "2.0.2"
     val unboundId               = "3.2.1"
     val jwt                     = "4.2.0"
