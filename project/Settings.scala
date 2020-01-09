6b8b7039f9eb8ae3e83bacee96c827b0ea389686
Scala Steward <me@scala-steward.org>
2020-01-09 23:18:42 +0100

    Update monocle-core, monocle-macro, ... to 2.0.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -54,7 +54,7 @@ object Settings {
     val janino                  = "3.1.0"
     val logstash                = "6.3"
     val pureConfig              = "0.12.2"
-    val monocleVersion          = "2.0.0"
+    val monocleVersion          = "2.0.1"
     val circeVersion            = "0.12.3"
     val doobieVersion           = "0.6.0"
     val flywayVersion           = "6.0.4"
