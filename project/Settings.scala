21870e1747b0e0d51a5cde2defa1924a1f15737d
Scala Steward <me@scala-steward.org>
2020-03-03 00:22:47 +0100

    Update diode to 1.1.8

--- project/Settings.scala
+++ project/Settings.scala
@@ -22,7 +22,7 @@ object Settings {
     val scalaDom                = "1.0.0"
     val scalajsReact            = "1.6.0"
     val booPickle               = "1.3.1"
-    val diode                   = "1.1.7"
+    val diode                   = "1.1.8"
     val diodeReact              = "1.1.7.160"
     val javaTimeJS              = "2.0.0-RC3"
     val scalaJQuery             = "1.2"
