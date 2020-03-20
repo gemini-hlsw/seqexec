01e88c5a9be416ae431a19eedaa7f4b573df1cc3
Scala Steward <me@scala-steward.org>
2020-03-20 09:27:15 +0100

    Update gmp-commands-jms-bridge to 0.6.3

--- project/Settings.scala
+++ project/Settings.scala
@@ -79,7 +79,7 @@ object Settings {
     val giapiStatusService      = "0.6.2"
     val gmpStatusGateway        = "0.3.2"
     val gmpStatusDatabase       = "0.3.2"
-    val gmpCmdClientBridge      = "0.6.2"
+    val gmpCmdClientBridge      = "0.6.3"
     val guava                   = "25.0-jre"
     val prometheusClient        = "0.8.1"
     val geminiLocales           = "0.3.0"
