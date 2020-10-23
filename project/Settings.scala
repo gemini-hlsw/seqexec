ba3082c73f5bdbd4a1dc284f1c9b7170b80e017f
Scala Steward <me@scala-steward.org>
2020-10-23 04:36:57 +0200

    Update react-virtualized to 0.11.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -17,7 +17,7 @@ object Settings {
     val javaTimeJS              = "2.0.0"
     val scalaJSReactCommon      = "0.10.0"
     val scalaJSSemanticUI       = "0.8.0"
-    val scalaJSReactVirtualized = "0.10.0"
+    val scalaJSReactVirtualized = "0.11.0"
     val scalaJSReactClipboard   = "1.3.0"
     val scalaJSReactDraggable   = "0.10.0"
     val scalaJSReactSortable    = "0.4.2"
