cf4fcf3424237fecc8d0918183108d9e37f596ef
Javier Lührs <jluhrs@gemini.edu>
2020-12-04 09:54:53 -0300

    SEQNG-1203 Implemented observing conditions check.

--- modules/seqexec/model/shared/src/main/scala/seqexec/model/UserPrompt.scala
+++ modules/seqexec/model/shared/src/main/scala/seqexec/model/UserPrompt.scala
@@ -4,6 +4,7 @@
 package seqexec.model
 
 import cats.Eq
+import cats.data.NonEmptyList
 import cats.syntax.all._
 import seqexec.model.Observation
 
@@ -19,18 +20,52 @@ object UserPrompt {
     case object WarningCancel extends PromptButtonColor
   }
 
-  implicit lazy val eq: Eq[UserPrompt] =
-    Eq.instance {
-      case (a: TargetCheckOverride, b: TargetCheckOverride) => a === b
-      case _                                                => false
-    }
+  sealed trait SeqCheck extends Product with Serializable;
 
-  // UserPrompt whether to override the target check
-  final case class TargetCheckOverride(sid: Observation.Id, stepId: StepId, obsTarget: String, tcsTarget: String)
-      extends UserPrompt
+  object SeqCheck {
+    implicit lazy val eq: Eq[SeqCheck] = Eq.instance {
+      case (a: TargetCheckOverride, b: TargetCheckOverride)               => a === b
+      case (a: ObsConditionsCheckOverride, b: ObsConditionsCheckOverride) => a === b
+      case _                                                              => false
+    }
+  }
+
+  final case class Discrepancy[A: Eq](actual: A, required: A)
+
+  object Discrepancy {
+    implicit def eq[A: Eq]: Eq[Discrepancy[A]] = Eq.by(x => (x.actual, x.required))
+  }
+
+  final case class TargetCheckOverride(self: Discrepancy[String])
+    extends SeqCheck
 
   object TargetCheckOverride {
     implicit lazy val eq: Eq[TargetCheckOverride] =
-      Eq.by(x => (x.sid, x.stepId, x.obsTarget, x.tcsTarget))
+      Eq.by(_.self)
   }
+
+  // UserPrompt whether to override the observing conditions
+  final case class ObsConditionsCheckOverride(cc: Option[Discrepancy[String]],
+                                              iq: Option[Discrepancy[String]],
+                                              sb: Option[Discrepancy[String]],
+                                              wv: Option[Discrepancy[String]]
+                                             ) extends SeqCheck
+
+  object ObsConditionsCheckOverride {
+    implicit lazy val eq: Eq[ObsConditionsCheckOverride] = Eq.by(x => (x.cc, x.iq, x.sb, x.wv))
+  }
+
+  implicit lazy val eq: Eq[UserPrompt] =
+    Eq.instance {
+      case (a: ChecksOverride, b: ChecksOverride) => a === b
+      case _                                      => false
+    }
+
+  // UserPrompt whether to override start checks
+  final case class ChecksOverride(sid: Observation.Id, stepId: StepId, checks: NonEmptyList[SeqCheck]) extends UserPrompt
+
+  object ChecksOverride {
+    implicit lazy val eq: Eq[ChecksOverride] = Eq.by(x => (x.sid, x.stepId, x.checks) )
+  }
+
 }
