e440bd86776aae7a1337788ab444cb28870165fc
Scala Steward <me@scala-steward.org>
2020-12-23 08:28:45 +0100

    Update scala-java-time to 2.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -14,7 +14,7 @@ object Settings {
     val scalajsReact            = "1.7.7"
     val booPickle               = "1.3.3"
     val diode                   = "1.1.14"
-    val javaTimeJS              = "2.0.0"
+    val javaTimeJS              = "2.1.0"
     val scalaJSReactCommon      = "0.11.2"
     val scalaJSSemanticUI       = "0.10.1"
     val scalaJSReactVirtualized = "0.11.2"
