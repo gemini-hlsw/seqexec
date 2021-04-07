bb579f4e25f78ce811f808287874fe0c14d8df41
Scala Steward <me@scala-steward.org>
2021-04-07 06:31:57 +0200

    Update gemini-locales to 0.6.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -76,7 +76,7 @@ object Settings {
     val gmpCmdClientBridge  = "0.6.7"
     val guava               = "30.1-jre"
     val prometheusClient    = "0.10.0"
-    val geminiLocales       = "0.5.1"
+    val geminiLocales       = "0.6.0"
     val pprint              = "0.6.4"
     val jaxb                = "2.3.1"
 
