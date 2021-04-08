8a5579c46a16f089ee183be00f175ee8fd9c8d45
Javier Lührs <jluhrs@gemini.edu>
2021-04-08 16:38:17 -0400

    SEQNG-1260 Added missing file headers.

--- modules/acm/src/test/scala/edu/gemini/epics/acm/ApplySenderSpec.scala
+++ modules/acm/src/test/scala/edu/gemini/epics/acm/ApplySenderSpec.scala
@@ -1,3 +1,6 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package edu.gemini.epics.acm
 
 import edu.gemini.epics.api.ChannelListener
