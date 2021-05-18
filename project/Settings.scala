a7f1b15fbaa2d4b7015408963eafdb70aee988ed
Scala Steward <me@scala-steward.org>
2021-05-18 04:30:34 +0200

    Update log4cats-core, log4cats-noop, ... to 2.1.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -39,7 +39,7 @@ object Settings {
     val jwt            = "5.0.0"
     val slf4j          = "1.7.30"
     val log4s          = "1.10.0"
-    val log4cats       = "1.3.1"
+    val log4cats       = "2.1.1"
     val log4catsLevel  = "0.2.0"
     val logback        = "1.2.3"
     val janino         = "3.1.4"
