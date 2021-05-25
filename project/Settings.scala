f65e450a7e0dc72a058c595296c5e28f1265869d
Scala Steward <me@scala-steward.org>
2021-05-25 18:27:26 +0200

    Update cats-testkit-scalatest to 2.1.5

--- project/Settings.scala
+++ project/Settings.scala
@@ -91,7 +91,7 @@ object Settings {
   object Libraries {
     // Test Libraries
     val TestLibs       = Def.setting(
-      "org.typelevel" %%% "cats-testkit-scalatest" % "2.1.4" % "test"
+      "org.typelevel" %%% "cats-testkit-scalatest" % "2.1.5" % "test"
     )
     val MUnit          = Def.setting(
       Seq(
