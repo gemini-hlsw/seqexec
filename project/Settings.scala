56eb359f9216ec08904b286ff0807218a7a8b968
Scala Steward <me@scala-steward.org>
2021-05-26 12:39:15 +0200

    Update circe-core, circe-generic, ... to 0.14.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -46,7 +46,7 @@ object Settings {
     val logstash       = "6.6"
     val pureConfig     = "0.14.1"
     val monocleVersion = "2.1.0"
-    val circeVersion   = "0.13.0"
+    val circeVersion   = "0.14.0"
     val doobieVersion  = "0.6.0"
     val flywayVersion  = "6.0.4"
 
