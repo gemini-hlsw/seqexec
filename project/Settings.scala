6a034b40d3be2896f463c18840b0994c4c84020d
Scala Steward <me@scala-steward.org>
2021-07-21 10:24:32 +0200

    Update react-semantic-ui to 0.11.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -16,7 +16,7 @@ object Settings {
     val diode                   = "1.1.14"
     val javaTimeJS              = "2.3.0"
     val scalaJSReactCommon      = "0.11.3"
-    val scalaJSSemanticUI       = "0.10.8"
+    val scalaJSSemanticUI       = "0.11.0"
     val scalaJSReactVirtualized = "0.11.3"
     val scalaJSReactClipboard   = "1.4.3"
     val scalaJSReactDraggable   = "0.11.3"
