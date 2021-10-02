5add1f95916aa9e7d96a6e2d755859095e80d84b
Scala Steward <me@scala-steward.org>
2021-10-03 00:59:59 +0200

    Update pureconfig, pureconfig-cats, ... to 0.17.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -44,7 +44,7 @@ object Settings {
     val logback        = "1.2.5"
     val janino         = "3.1.6"
     val logstash       = "6.6"
-    val pureConfig     = "0.14.1"
+    val pureConfig     = "0.17.0"
     val monocleVersion = "2.1.0"
     val circeVersion   = "0.14.1"
     val doobieVersion  = "0.6.0"
