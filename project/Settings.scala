bf9389841b3dd3c6cb17129ab5ead372d5aecfdb
Scala Steward <me@scala-steward.org>
2022-02-10 00:32:40 +0100

    Update http4s-async-http-client, ... to 0.21.32

--- project/Settings.scala
+++ project/Settings.scala
@@ -32,7 +32,7 @@ object Settings {
     val scalaXmlVerson      = "1.2.0"
     val catsTime            = "0.3.4"
 
-    val http4sVersion  = "0.21.31"
+    val http4sVersion  = "0.21.32"
     val squants        = "1.8.3"
     val commonsHttp    = "2.0.2"
     val unboundId      = "3.2.1"
