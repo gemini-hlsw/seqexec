5c7c02bc0afd45257f91ac029082aacd8e75ce3b
Scala Steward <me@scala-steward.org>
2022-04-12 10:20:49 +0200

    Update scalajs-react:core, ... to 2.1.0

--- project/Settings.scala
+++ project/Settings.scala
@@ -11,7 +11,7 @@ object Settings {
   object LibraryVersions {
     // ScalaJS libraries
     val scalaDom                = "1.2.0"
-    val scalajsReact            = "1.7.7"
+    val scalajsReact            = "2.1.0"
     val booPickle               = "1.4.0"
     val diode                   = "1.1.14"
     val javaTimeJS              = "2.3.0"
