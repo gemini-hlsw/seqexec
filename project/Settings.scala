b1c8809de862e778ea1a055b895c2d7391155bba
Scala Steward <me@scala-steward.org>
2022-05-05 06:17:06 +0200

    Update log4cats-loglevel to 0.3.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -40,7 +40,7 @@ object Settings {
     val slf4j          = "1.7.36"
     val log4s          = "1.10.0"
     val log4cats       = "1.4.0"
-    val log4catsLevel  = "0.2.0"
+    val log4catsLevel  = "0.3.1"
     val logback        = "1.2.11"
     val janino         = "3.1.7"
     val pureConfig     = "0.14.1"
