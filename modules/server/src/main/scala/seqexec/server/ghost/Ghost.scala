76952f0678b34d6efc2119cd11d7e23d0ab1e60a
Carlos Quiroz <cquiroz@gemini.edu>
2023-04-15 20:26:56 -0400

    Even more timeout padding

--- modules/server/src/main/scala/seqexec/server/ghost/Ghost.scala
+++ modules/server/src/main/scala/seqexec/server/ghost/Ghost.scala
@@ -54,7 +54,7 @@ final case class Ghost[F[_]: Logger: Async](
     with InstrumentSystem[F]
     with GhostLUT {
 
-  val readOutTimeExtra: Time = Seconds(40)
+  val readOutTimeExtra: Time = Seconds(240)
 
   override val gdsClient: GdsClient[F] = controller.gdsClient
 
