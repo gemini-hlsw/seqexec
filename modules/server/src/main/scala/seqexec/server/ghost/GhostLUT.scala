62accddcf4ccb7e645c81e0c4c71e677e8e6cfe1
Carlos Quiroz <cquiroz@gemini.edu>
2023-04-17 19:53:33 -0400

    Add an extra entry to SVCamera times for very faint targets

--- modules/server/src/main/scala/seqexec/server/ghost/GhostLUT.scala
+++ modules/server/src/main/scala/seqexec/server/ghost/GhostLUT.scala
@@ -106,7 +106,8 @@ trait GhostLUT {
       SVCameraTimes(18.5, 300.0, 300.0),
       SVCameraTimes(19.0, 300.0, 300.0),
       SVCameraTimes(19.5, 600.0, 600.0),
-      SVCameraTimes(20.0, 600.0, 600.0)
+      SVCameraTimes(20.0, 600.0, 600.0),
+      SVCameraTimes(24.0, 600.0, 600.0)
     ).sortBy(_.gMag)
 
   // the List is never empty
