8fed1d2778e6b7576bbddb6d8c75b755c1ed3104
Carlos Quiroz <cquiroz@gemini.edu>
2023-11-29 12:22:23 -0300

    Reformat

--- modules/server/src/main/scala/seqexec/server/tcs/TcsControllerEpicsCommon.scala
+++ modules/server/src/main/scala/seqexec/server/tcs/TcsControllerEpicsCommon.scala
@@ -466,7 +466,7 @@ object TcsControllerEpicsCommon {
         current,
         d,
         (x: Length) =>
-          Logger[F].debug( s"Set defocus to $x") *> epicsSys.instrumentDefocusCmd
+          Logger[F].debug(s"Set defocus to $x") *> epicsSys.instrumentDefocusCmd
             .setDefocus(x) <* Logger[F].debug("Set defocus done"),
         l.andThen(BaseEpicsTcsConfig.defocusB)
       )("defocus")
