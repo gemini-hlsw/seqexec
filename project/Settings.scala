3493c464e4ce30cbeb65830f342e175ea48f88a0
Carlos Quiroz <cquiroz@gemini.edu>
2023-12-01 12:53:09 -0300

    Update lucuma-core

--- project/Settings.scala
+++ project/Settings.scala
@@ -88,7 +88,7 @@ object Settings {
     val jaxb                = "2.3.1"
 
     // Gemini Libraries
-    val lucumaCore    = "0.45.0-1-69bba13-SNAPSHOT"
+    val lucumaCore    = "0.45.0-1-69bba13-20231201T155939Z-SNAPSHOT"
     val lucumaUI      = "0.39.0"
     val lucumaSchemas = "0.33.0"
 
