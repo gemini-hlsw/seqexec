51e42b0c31323f824a2a14d096af71fe3e3de101
Carlos Quiroz <cquiroz@gemini.edu>
2026-05-05 17:52:22 -0400

    Force move ifu to focus for calibrations

--- modules/server/src/main/scala/seqexec/server/ghost/GhostConfig.scala
+++ modules/server/src/main/scala/seqexec/server/ghost/GhostConfig.scala
@@ -256,7 +256,7 @@ sealed trait GhostConfig extends GhostLUT {
     baseConfiguration |+| slitMaskConfiguration |+| (
       if (!isScience(obsType)) {
         // as per REL-4855 we move ifus to focus for calibrations
-        ifuCalibration |+| channelConfig |+| svCalib |+|
+        ifuCalibration |+| channelConfig |+| moveIFUToFocus |+| svCalib |+|
           GhostConfig.fiberConfig1(fiberAgitator1) |+|
           GhostConfig.fiberConfig2(fiberAgitator2)
       } else
